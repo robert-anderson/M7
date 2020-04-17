75ac95c6582785422a2f3b4b249948f5f7682c4a
Robert Anderson <robert.anderson@kcl.ac.uk>
2020-04-17 22:46:14 +0100

    adding prof target

--- src/core/io/FileIterator.cpp
+++ src/core/io/FileIterator.cpp
@@ -8,7 +8,7 @@
 FileIterator::FileIterator(const std::string &filename, const size_t &ifirstline):
         m_file(new std::ifstream(filename)), m_ifirstline(ifirstline) {
     for (size_t i=0ul; i<m_ifirstline; i++) next();
-    assert(m_file->is_open());
+    if (!m_file->is_open()) throw std::runtime_error("File \""+filename+"\" not found.");
 }
 
 FileIterator::FileIterator(const std::string &filename):
