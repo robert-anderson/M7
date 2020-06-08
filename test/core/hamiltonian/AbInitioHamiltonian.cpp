1579e350f12ccd521bef53285b93498ba0075c1c
Robert John Anderson <robert.anderson@kcl.ac.uk>
2020-06-08 02:30:10 +0100

    skipping complex tests when compiling with real

--- test/core/hamiltonian/AbInitioHamiltonian.cpp
+++ test/core/hamiltonian/AbInitioHamiltonian.cpp
@@ -7,6 +7,7 @@
 #include "src/core/hamiltonian/AbInitioHamiltonian.h"
 
 TEST(AbInitioHamiltonian, DhfEnergy) {
+    if (!consts::is_complex<defs::ham_t>()) GTEST_SKIP();
     const auto benchmark = -14.354220448530139;
     AbInitioHamiltonian ham(defs::assets_root + "/DHF_Be_STO-3G/FCIDUMP");
     Determinant hf_det(ham.nsite());
@@ -18,6 +19,7 @@ TEST(AbInitioHamiltonian, DhfEnergy) {
 }
 
 TEST(AbInitioHamiltonian, DhfBrillouinTheorem) {
+    if (!consts::is_complex<defs::ham_t>()) GTEST_SKIP();
     AbInitioHamiltonian ham(defs::assets_root + "/DHF_Be_STO-3G/FCIDUMP");
     Determinant hf_det(ham.nsite());
     hf_det.set(defs::inds{0, 1, ham.nsite(), ham.nsite() + 1});
@@ -61,4 +63,30 @@ TEST(AbInitioHamiltonian, RhfEnergy) {
     ASSERT_TRUE(consts::floats_equal(consts::real(elem), benchmark));
     ASSERT_TRUE(consts::float_nearly_zero(consts::imag(elem), 1e-14));
     ASSERT_TRUE(consts::floats_equal(ham.get_energy(hf_det), benchmark));
+}
+
+
+TEST(AbInitioHamiltonian, RhfBrillouinTheorem) {
+    AbInitioHamiltonian ham(defs::assets_root + "/RHF_N2_6o6e/FCIDUMP");
+    Determinant hf_det(ham.nsite());
+    hf_det.set(defs::inds{0, 1, 2,  6, 7, 8});
+    size_t removed, inserted;
+
+    OccupiedOrbitals occs(hf_det);
+    VacantOrbitals vacs(hf_det);
+
+    Determinant excited(ham.nsite());
+
+    AntisymConnection connection(hf_det);
+
+    for (size_t iocc = 0ul; iocc < occs.m_nind; ++iocc) {
+        const auto &occ = occs.m_inds[iocc];
+        for (size_t ivac = 0ul; ivac < vacs.m_nind; ++ivac) {
+            const auto &vac = vacs.m_inds[iocc];
+            connection.zero();
+            connection.add(occ, vac);
+            connection.apply(hf_det);
+            ASSERT_TRUE(consts::float_is_zero(ham.get_element_1(connection)));
+        }
+    }
 }
