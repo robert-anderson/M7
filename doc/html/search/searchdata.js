e1a80f8e7b07007a187db3b5b30648057b6346da
Teamcity <teamcity@zombie.client.kcl.ac.uk>
2020-06-22 16:02:44 +0100

    reusing README.md from root directory for docs

--- doc/html/search/searchdata.js
+++ doc/html/search/searchdata.js
@@ -1,18 +1,21 @@
 var indexSectionsWithContent =
 {
   0: "abcdefhilmnoprstvw",
-  1: "abcdefhilmnoprstvw"
+  1: "abcdefhilmnoprstvw",
+  2: "m"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes"
+  1: "classes",
+  2: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Classes"
+  1: "Classes",
+  2: "Pages"
 };
 
