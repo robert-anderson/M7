bfc91c6f1e707379908be272890cd602ef2b6ae6
James Halson <james.halson@kcl.ac.uk>
2020-09-30 13:45:58 +0100

    added half a test for PermanentConnection.cpp

--- test/core/basis/PermanentConnection.cpp
+++ test/core/basis/PermanentConnection.cpp
@@ -4,4 +4,20 @@
 
 #include "gtest/gtest.h"
 #include "src/core/basis/PermanentConnection.h"
+#include "src/core/basis/Permanent.h"
 
+TEST(PermanentConnection, Occupation){
+//  size_t nmode = 4ul;
+//  size_t occ_cutoff = 6ul;
+//
+//  // permanent -> nmode-length array of integers <= occ_cutoff
+//  Permanent ket(nmode, occ_cutoff);
+//  Permanent bra(nmode, occ_cutoff);
+//
+//  // TODO setup occs of ket and bra
+//
+//  PermanentConnection pc(ket, bra);
+//
+//  // TODO things to test... pc(k,b) == pc(b,k)
+//  //
+}
