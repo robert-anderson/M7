78f956bafcc693828700365940d423711f47e318
James Halson <james.halson@kcl.ac.uk>
2020-10-01 15:58:30 +0100

    for bug repro

--- test/core/basis/PermanentConnection.cpp
+++ test/core/basis/PermanentConnection.cpp
@@ -6,35 +6,81 @@
 #include "src/core/basis/PermanentConnection.h"
 #include "src/core/basis/Permanent.h"
 
-TEST(PermanentConnection, Occupation){
+TEST(PermanentConnection, SingleChange){
     size_t nmode = 4ul;
     size_t occ_cutoff = 6ul;
 
-    // permanent -> nmode-length array of integers <= occ_cutoff
     Permanent ket(nmode, occ_cutoff);
     Permanent bra(nmode, occ_cutoff);
 
-    ket(0) = 9;
+    ket(0) = 2;
     ket(1) = 4;
-    ket(2) = 5;
-    ket(3) = 12;
+    ket(2) = 0;
+    ket(3) = 1;
 
-    bra(0) = 9;
+    bra(0) = 2;
     bra(1) = 4;
-    bra(2) = 5;
-    bra(3) = 13;
+    bra(2) = 0;
+    bra(3) = 1;
 
-    //for (auto& item : ket.to_vector()) std::cout << (int)item << std::endl;
+    std::cout << "testing initial boson occupation vector with elements:" << std::endl;
+    for (auto& item : ket.to_vector()) std::cout << (int)item << std::endl;
+    ASSERT(0)
 
-    // TODO setup occs of ket and bra
-    PermanentConnection pc(ket, bra);
-    ASSERT_EQ(pc.nchanged_mode(), 1);
-    ASSERT_EQ(pc.changed_modes()[0], 3);
-    ASSERT_EQ(pc.changes()[pc.changed_modes()[0]], -1);
 
-    bra(3) = 10;
-    pc.connect(ket, bra);
-    ASSERT_EQ(pc.changes()[pc.changed_modes()[0]], 2);
 
-    // TODO things to test... pc(k,b) == pc(b,k)
+
+    for(size_t imode = 0; imode < nmode; ++imode){
+        for (size_t idelta = 0; idelta < occ_cutoff; ++idelta) {
+            PermanentConnection pc(ket, bra);
+
+            bra(imode) += idelta;
+            pc.connect(ket, bra);
+
+            ASSERT_EQ(pc.nchanged_mode(), 1);
+            ASSERT_EQ(pc.changed_modes()[0], imode);
+            ASSERT_EQ(pc.changes()[pc.changed_modes()[0]], idelta);
+
+            }
+        }
+}
+
+TEST(PermanentConnection, DoubleChange){
+    size_t nmode = 4ul;
+    size_t occ_cutoff = 6ul;
+
+    Permanent ket(nmode, occ_cutoff);
+    Permanent bra(nmode, occ_cutoff);
+
+    ket(0) = 2;
+    ket(1) = 4;
+    ket(2) = 0;
+    ket(3) = 1;
+
+    bra(0) = 2;
+    bra(1) = 4;
+    bra(2) = 0;
+    bra(3) = 1;
+
+    //std::cout << "testing initial boson occupation vector with elements:" << std::endl;
+    for (auto& item : ket.to_vector()) std::cout << (int)item << std::endl;
+
+    for(size_t imode1 = 0; imode1 < nmode; ++imode1){
+        for(size_t imode2 = 0; imode2 < imode1; ++imode2){
+            for (size_t idelta1 = 0; idelta1 < occ_cutoff; ++idelta1) {
+                for (size_t idelta2 = 0; idelta2 < occ_cutoff; ++idelta2) {
+                    PermanentConnection pc(ket, bra);
+                    bra(imode1) += idelta1;
+                    bra(imode2) += idelta2;
+                    pc.connect(ket, bra);
+
+                    ASSERT_EQ(pc.nchanged_mode(), 2);
+                    ASSERT_EQ(pc.changed_modes()[0], imode1);
+                    ASSERT_EQ(pc.changed_modes()[2], imode2);
+                    ASSERT_EQ(pc.changes()[pc.changed_modes()[0]], idelta1);
+                    ASSERT_EQ(pc.changes()[pc.changed_modes()[1]], idelta2);
+                }
+        }
+    }
+}
 }
---
+++ test/core/hamiltonian/BosonCouplings.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jhalson on 30/09/2020.
+//
+
+
