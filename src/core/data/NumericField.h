c736a7d4830b7a6e6222f095106a59dcf97c7b3d
Robert John Anderson <robert.anderson@kcl.ac.uk>
2020-10-02 21:36:35 +0100

    Removing the obsolete NumericField::View, and replacing with references

--- src/core/data/NumericField.h
+++ src/core/data/NumericField.h
@@ -12,30 +12,10 @@
 
 template<typename T, size_t nind>
 struct NumericField : public Field<nind> {
-    struct View : Field<nind>::View {
-        using Field<nind>::View::m_ptr;
-        View(const NumericField<T, nind>& field, const size_t& irow, const size_t& ielement):
-                Field<nind>::View(field.begin(irow)+ielement*sizeof(T)){
-        }
-        operator T&() {
-            return *((T*)m_ptr);
-        }
-        operator const T&() const {
-            return *((T*)m_ptr);
-        }
-        View& operator =(const T& v){
-            *((T*)m_ptr) = v;
-            return *this;
-        }
-        std::string to_string() const {
-            return utils::num_to_string((T)*this);
-        }
-    };
-
     using FieldBase::m_nelement;
     std::string to_string(size_t irow) const override {
         std::string res;
-        for (size_t i=0ul; i<m_nelement; ++i) res+=View(*this, irow, i).to_string()+" ";
+        for (size_t i=0ul; i<m_nelement; ++i) res+=utils::num_to_string(flat_get(irow, i))+" ";
         return res;
     }
 
@@ -45,10 +25,31 @@ struct NumericField : public Field<nind> {
         FieldBase::set_offsets();
     }
 
+private:
+    /*
+     * Flat accessors
+     */
+    inline T& flat_get(const size_t& irow, size_t ielement){
+        return ((T*)FieldBase::begin(irow))[ielement];
+    }
+    inline const T& flat_get(const size_t& irow, size_t ielement) const{
+        return ((T*)FieldBase::begin(irow))[ielement];
+    }
+
     using Field<nind>::m_format;
+
+public:
+    /*
+     * Formatted accessors
+     */
     template<typename ...Args>
-    View operator()(const size_t& irow, Args... inds){
-        return View(*this, irow, m_format.flat(inds...));
+    T& operator()(const size_t& irow, Args... inds){
+        return flat_get(irow, m_format.flat(inds...));
+    }
+
+    template<typename ...Args>
+    const T& operator()(const size_t& irow, Args... inds) const {
+        return ((T*)FieldBase::begin(irow))[m_format.flat(inds...)];
     }
 };
 
