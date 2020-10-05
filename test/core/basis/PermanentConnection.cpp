d73d8864dfefc07efb24e568adb9f2d06c4bd216
James Halson <james.halson@kcl.ac.uk>
2020-10-05 15:22:18 +0100

    for bug repro II

--- test/core/basis/PermanentConnection.cpp
+++ test/core/basis/PermanentConnection.cpp
@@ -13,34 +13,26 @@ TEST(PermanentConnection, SingleChange){
     Permanent ket(nmode, occ_cutoff);
     Permanent bra(nmode, occ_cutoff);
 
-    ket(0) = 2;
-    ket(1) = 4;
-    ket(2) = 0;
-    ket(3) = 1;
-
-    bra(0) = 2;
-    bra(1) = 4;
-    bra(2) = 0;
-    bra(3) = 1;
-
-    std::cout << "testing initial boson occupation vector with elements:" << std::endl;
-    for (auto& item : ket.to_vector()) std::cout << (int)item << std::endl;
-    ASSERT(0)
-
-
-
-
     for(size_t imode = 0; imode < nmode; ++imode){
         for (size_t idelta = 0; idelta < occ_cutoff; ++idelta) {
             PermanentConnection pc(ket, bra);
 
+            ket(0) = 2;
+            ket(1) = 4;
+            ket(2) = 0;
+            ket(3) = 1;
+
+            bra(0) = 2;
+            bra(1) = 4;
+            bra(2) = 0;
+            bra(3) = 1;
+
             bra(imode) += idelta;
-            pc.connect(ket, bra);
+            pc.connect(bra, ket);
 
-            ASSERT_EQ(pc.nchanged_mode(), 1);
-            ASSERT_EQ(pc.changed_modes()[0], imode);
+            ASSERT_EQ(pc.nchanged_mode(), idelta == 0 ? 0 : 1);
+            ASSERT_EQ(pc.changed_modes()[0], idelta == 0 ? 0 : imode);
             ASSERT_EQ(pc.changes()[pc.changed_modes()[0]], idelta);
-
             }
         }
 }
