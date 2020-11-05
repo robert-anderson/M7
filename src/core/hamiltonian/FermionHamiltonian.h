55247b92e08a6cba1eb38d66bf0516076ed1ac93
Robert John Anderson <robert.anderson@kcl.ac.uk>
2020-11-05 17:49:00 +0000

    moving method defs to implementation file

--- src/core/hamiltonian/FermionHamiltonian.h
+++ src/core/hamiltonian/FermionHamiltonian.h
@@ -27,111 +27,37 @@ protected:
 
 public:
     FermionHamiltonian(const size_t &nelec, const size_t &nsite, bool spin_conserving_1e, bool spin_conserving_2e,
-                       bool complex_valued, bool spin_resolved) :
-            m_nelec(nelec), m_nsite(nsite),
-            m_spin_conserving_1e(spin_conserving_1e),
-            m_spin_conserving_2e(spin_conserving_2e),
-            m_complex_valued(complex_valued),
-            m_int_1(nsite, spin_resolved),
-            m_int_2(nsite, spin_resolved)
-            {}
+                       bool complex_valued, bool spin_resolved);
 
-    FermionHamiltonian(const FcidumpFileReader<defs::ham_t> &file_reader) :
-            FermionHamiltonian(file_reader.nelec(), file_reader.nspatorb(),
-                               file_reader.spin_conserving_1e(),
-                               file_reader.spin_conserving_2e(),
-                               file_reader.m_complex_valued, file_reader.spin_resolved()) {
-        defs::inds inds(4);
-        defs::ham_t value;
-
-        logger::write("Loading ab-initio FermionHamiltonian from FCIDUMP...");
-        while (file_reader.next(inds, value)) {
-            if (ints2_t::valid_inds(inds)) m_int_2.set(inds, value);
-            else if (ints1_t::valid_inds(inds)) m_int_1.set(inds, value);
-            else if (inds[0] == ~0ul) m_int_0 = value;
-        }
-        mpi::barrier();
-        logger::write("FCIDUMP loading complete.");
-    }
+    FermionHamiltonian(const FcidumpFileReader<defs::ham_t> &file_reader);
 
 
-    explicit FermionHamiltonian(const std::string& fname, bool spin_major):
-            FermionHamiltonian(FcidumpFileReader<defs::ham_t>(fname, spin_major)){}
+    FermionHamiltonian(const std::string& fname, bool spin_major);
 
-    consts::component_t<defs::ham_t>::type get_energy(const views::FermionOnv &det) const {
-        return consts::real(get_element_0(det));
-    }
+    consts::component_t<defs::ham_t>::type get_energy(const views::FermionOnv &det) const;
 
-    defs::ham_t get_element_0(const defs::det_work &occs, const size_t &nocc) const {
-        defs::ham_t element = m_int_0;
-        for (size_t i = 0ul; i < nocc; ++i) {
-            auto const &occi = occs[i];
-            element += m_int_1(occi, occi);
-            for (size_t j = 0ul; j < i; ++j) {
-                auto const &occj = occs[j];
-                element += m_int_2.phys_antisym_element(occi, occj, occi, occj);
-            }
-        }
-        return element;
-    }
+    defs::ham_t get_element_0(const defs::det_work &occs, const size_t &nocc) const;
 
-    defs::ham_t get_element_0(const OccupiedOrbitals &occs) const {
-        return get_element_0(occs.m_inds, occs.m_nind);
-    }
+    defs::ham_t get_element_0(const OccupiedOrbitals &occs) const;
 
-    defs::ham_t get_element_0(const views::FermionOnv &det) const {
-        OccupiedOrbitals occs(det);
-        return get_element_0(occs.m_inds, occs.m_nind);
-    }
+    defs::ham_t get_element_0(const views::FermionOnv &det) const;
 
-    defs::ham_t get_element_0(const AntisymConnection &connection) const {
-        return get_element_0(connection.com(), connection.ncom());
-    }
+    defs::ham_t get_element_0(const AntisymConnection &connection) const;
 
-    defs::ham_t get_element_1(const AntisymConnection &connection) const {
-        const auto &cre = connection.cre(0);
-        const auto &ann = connection.ann(0);
-        const auto &coms = connection.com();
-        const auto &ncom = connection.ncom();
+    defs::ham_t get_element_1(const AntisymConnection &connection) const;
 
-        defs::ham_t element = m_int_1(cre, ann);
-        for (size_t icom = 0ul; icom < ncom; ++icom)
-            element += m_int_2.phys_antisym_element(cre, coms[icom], ann, coms[icom]);
-        return connection.phase() ? -element : element;
-    }
+    defs::ham_t get_element_2(const size_t &i, const size_t &j, const size_t &k, const size_t &l) const;
 
-    defs::ham_t get_element_2(const size_t &i, const size_t &j, const size_t &k, const size_t &l) const {
-        return m_int_2.phys_antisym_element(i, j, k, l);
-    }
+    defs::ham_t get_element_2(const DeterminantConnection &connection) const;
 
-    defs::ham_t get_element_2(const DeterminantConnection &connection) const {
-        return get_element_2(connection.cre(0), connection.cre(1), connection.ann(0), connection.ann(1));
-    }
+    defs::ham_t get_element_2(const AntisymConnection &connection) const;
 
-    defs::ham_t get_element_2(const AntisymConnection &connection) const {
-        const auto element = get_element_2(connection.cre(0), connection.cre(1), connection.ann(0), connection.ann(1));
-        return connection.phase() ? -element : element;
-    }
+    defs::ham_t get_element(const AntisymConnection &connection) const;
 
-    defs::ham_t get_element(const AntisymConnection &connection) const {
-        switch (connection.nexcit()) {
-            case 0:
-                return get_element_0(connection);
-            case 1: ASSERT(connection.ncom() + connection.nexcit() == nelec());
-                return get_element_1(connection);
-            case 2:
-                return get_element_2(connection);
-            default:
-                return 0;
-        }
-    }
-
-    defs::ham_t get_element(const views::FermionOnv &bra, const views::FermionOnv &ket) const {
-        return get_element(AntisymConnection(ket, bra));
-    }
+    defs::ham_t get_element(const views::FermionOnv &bra, const views::FermionOnv &ket) const;
 
     size_t nci() const {
-        return integer_utils::combinatorial(2 * nsite(), nelec());
+        return ci_utils::fermion_dim(nsite(), nelec());
     }
 
     const size_t &nsite() const {
