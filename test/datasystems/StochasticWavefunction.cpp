347896e5c6bf0cd67d7a59f1f60e07f20b8d5b7d
Robert John Anderson <robert.anderson@kcl.ac.uk>
2020-11-16 22:51:41 +0000

    removing old tests

--- test/datasystems/StochasticWavefunction.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Robert John Anderson on 2020-02-11.
-//
-#include "gtest/gtest.h"
-//#include "src/dynamics/Wavefunction.h"
-
-TEST(StochasticWavefunction, test){
-    //Wavefunction swf;
-}
