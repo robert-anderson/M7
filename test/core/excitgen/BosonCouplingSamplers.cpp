b296c22e109783f4035bc724866c4d1c88e4f230
James Halson <james.halson@kcl.ac.uk>
2020-11-17 16:51:51 +0000

    boson excitgen tests passing

--- test/core/excitgen/BosonCouplingSamplers.cpp
+++ test/core/excitgen/BosonCouplingSamplers.cpp
@@ -19,50 +19,111 @@ namespace boson_coupling_samplers_test {
                 m_onv(this, {nsite, nsite}, "occupation number vector"),
                 m_frequency(this, "number of times the ONV was drawn"),
                 m_weight(this, "cumulative reciprocal probability") {}
+
+
+        bool test_boson_gen(elements::FermiBosOnv &src_onv, size_t ndraw, size_t nboson_max) {
+            size_t nsite = src_onv.m_fonv.nsite();
+            BufferedTable<boson_coupling_samplers_test::TestTable> bt("Excit gen tester", nsite);
+
+            populate_table(bt, src_onv, ndraw, nboson_max);
+            /*
+             * TODO: James
+             *  ASSERT that the m_weights are approximately the same,
+             *  would be better to refactor the above into a function that can be called
+             *  multiple times with different ndraw values, and check that the variance
+             *  decreases as expected
+             *  ASSERT that all expected excitations are generated at least once (by counting,
+             *  i.e. check the m_hwm member of bt is correct)
+             */
+            bool result = all_have_at_least_1(src_onv, nboson_max, bt.m_hwm);
+            result &= errors_decreasing(src_onv, ndraw, nboson_max);
+            result &= is_uniform(bt.m_frequency, ndraw);
+            return result;
+        }
+
+        void populate_table(BufferedTable<boson_coupling_samplers_test::TestTable> &bt,
+                            elements::FermiBosOnv &src_onv,
+                            size_t ndraw,
+                            size_t nboson_max) {
+            size_t nsite = src_onv.m_fonv.nsite();
+            elements::FermiBosOnv dst_onv(nsite, nsite);
+            PRNG prng = PRNG(18, 1e4);
+            BosonCouplings bc(nsite, nboson_max, 1.0, 0.5);
+            BosonCouplingSamplers sampler(bc, nboson_max, prng);
+            OccupiedOrbitals occ_orbs(src_onv.m_fonv);
+            conn::AsFermiBosOnv aconn(src_onv);
+            defs::prob_t prob;
+            defs::ham_t helem;
+            for (size_t idraw = 0ul; idraw < ndraw; ++idraw) {
+                sampler.draw_single(src_onv, dst_onv, occ_orbs, prob, helem, aconn);
+                auto irow = *bt[dst_onv];
+                if (irow == ~0ul) {
+                    bt.expand(1);
+                    irow = bt.insert(dst_onv);
+                }
+                bt.m_frequency(irow)++;
+                bt.m_weight(irow) += 1.0 / prob;
+            }
+        }
+
+        bool all_have_at_least_1(elements::FermiBosOnv &src_onv, size_t nboson_max, size_t num_generated) {
+            // check correct no. of excitations is generated.
+            size_t num_expected = 0;
+            for (size_t imode = 0; imode < src_onv.m_bonv.nmode(); ++imode) {
+                if(src_onv.m_fonv.get(0, imode) or src_onv.m_fonv.get(1, imode)){
+                    num_expected++;
+                    num_expected += (src_onv.m_bonv(imode) != 0 and src_onv.m_bonv(imode) != nboson_max);
+                }
+            }
+            return num_expected == num_generated;
+        }
+
+        bool
+        errors_decreasing(elements::FermiBosOnv &src_onv, size_t ndraw_init, size_t nboson_max, size_t ntries = 3) {
+            BufferedTable<boson_coupling_samplers_test::TestTable> bt("Error tester", src_onv.m_fonv.nsite());
+            std::vector<defs::prob_t> vars(2 * src_onv.m_bonv.nmode(), 1000);
+            for (size_t itry = 0; itry < ntries; ++itry) {
+                size_t ndraw = (size_t) ndraw_init * std::pow(100, itry);
+                populate_table(bt, src_onv, ndraw, nboson_max);
+                for (size_t irow = 0; irow < bt.m_hwm; ++irow) {
+                    auto this_var = std::abs(1.0 - bt.m_weight(irow) / ndraw);
+                    if (this_var > vars[irow]) return false;
+                    vars[irow] = this_var;
+                }
+            }
+            return true;
+        }
+
+        bool is_uniform(fields::Number<size_t> &freqs, size_t ndraw, double tol = 2.0) {
+            size_t min_freq = ~0ul;
+            size_t max_freq = 0;
+            for (size_t ifreq = 0; ifreq < freqs.m_field.m_nelement; ++ifreq) {
+                auto this_freq = freqs(ifreq);
+                min_freq = this_freq < min_freq ? this_freq : min_freq;
+                max_freq = this_freq > max_freq ? this_freq : max_freq;
+            }
+            return std::abs(max_freq - (double) min_freq) < std::sqrt(ndraw / 12.0) / tol;
+        }
     };
 }
+
+
 
 TEST(BosonCouplingSamplers, SingleOnvTest){
     const size_t nsite = 6;
     const size_t nboson_max = 3;
-    BufferedTable<boson_coupling_samplers_test::TestTable> bt("Excit gen tester", nsite);
 
     const size_t ndraw = 100000;
-
     elements::FermiBosOnv src_onv(nsite, nsite);
-    elements::FermiBosOnv dst_onv(nsite, nsite);
     src_onv = {{0, 4, 6, 11}, {1, 0, 0, 1, 3, 2}};
-    // 5
 
-    //std::cout << src_onv.to_string() << std::endl;
+    boson_coupling_samplers_test::TestTable tt(nsite);
 
-    PRNG prng = PRNG(18, 1e4);
-    BosonCouplings bc(nsite, nboson_max, 1.0, 0.5);
-    BosonCouplingSamplers sampler(bc, nboson_max, prng);
-    OccupiedOrbitals occ_orbs(src_onv.m_fonv);
-    conn::AsFermiBosOnv aconn(src_onv);
-    defs::prob_t prob;
-    defs::ham_t helem;
-    for (size_t idraw=0ul; idraw<ndraw; ++idraw){
-        sampler.draw_single(src_onv, dst_onv, occ_orbs, prob, helem, aconn);
-        auto irow = *bt[dst_onv];
-        if (irow==~0ul){
-            bt.expand(1);
-            irow = bt.insert(dst_onv);
-        }
-        bt.m_frequency(irow)++;
-        bt.m_weight(irow)+=1.0/prob;
-    }
-    /*
-     * TODO: James
-     *  ASSERT that the m_weights are approximately the same,
-     *  would be better to refactor the above into a function that can be called
-     *  multiple times with different ndraw values, and check that the variance
-     *  decreases as expected
-     *  ASSERT that all expected excitations are generated at least once (by counting,
-     *  i.e. check the m_hwm member of bt is correct)
-     */
-    for(size_t irow = 0ul; irow < bt.m_hwm; ++irow){
-        std::cout << std::abs(1.0 - bt.m_weight(irow)/ndraw) << std::endl;
-    }
+    tt.test_boson_gen(src_onv, ndraw, nboson_max);
+
+
+
+
+
+
 }
