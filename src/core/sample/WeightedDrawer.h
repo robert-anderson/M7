61460fab3555a5508f28f192182355e91fa19e2f
Robert Anderson <robert.anderson@kcl.ac.uk>
2020-11-20 14:06:25 +0000

    WeightedDrawer.h and test added
    for abstraction of probability hierarchies.

---
+++ src/core/sample/WeightedDrawer.h
@@ -0,0 +1,43 @@
+//
+// Created by RJA on 20/11/2020.
+//
+
+#ifndef M7_WEIGHTEDDRAWER_H
+#define M7_WEIGHTEDDRAWER_H
+
+#include "PRNG.h"
+
+class WeightedDrawer {
+
+    void update_cumprobs();
+
+    void set_one(size_t iprob, defs::prob_t prob);
+
+    template<typename ...Args>
+    void set_one(size_t iprob, defs::prob_t first, Args... rest){
+        m_probs[iprob] = first;
+        set_one(iprob+1, rest...);
+    }
+
+public:
+    std::vector<defs::prob_t> m_probs;
+    std::vector<defs::prob_t> m_cumprobs;
+    PRNG& m_prng;
+
+    WeightedDrawer(size_t nprob, PRNG& prng);
+
+    template<typename ...Args>
+    void set(Args... probs){
+        ASSERT(sizeof...(Args)+1>=m_probs.size());
+        set_one(0, probs...);
+        update_cumprobs();
+    }
+
+    size_t operator()();
+
+    const defs::prob_t& prob(size_t iprob);
+
+};
+
+
+#endif //M7_WEIGHTEDDRAWER_H
---
+++ test/core/sample/WeightedDrawer.cpp
@@ -0,0 +1,23 @@
+//
+// Created by RJA on 20/11/2020.
+//
+
+#include "gtest/gtest.h"
+#include "src/core/sample/WeightedDrawer.h"
+
+TEST(WeightedDrawer, Test) {
+    PRNG prng(14, 1000);
+    WeightedDrawer drawer(4, prng);
+    drawer.set(0.2, 0.4, 0.05);
+    ASSERT_FLOAT_EQ(drawer.prob(3), 0.35);
+    drawer.set(0.2, 0.4, 0.1, 0.3);
+    defs::inds freqs(4, 0ul);
+    const size_t ndraw = 100000000;
+    for (size_t idraw=0ul; idraw<ndraw; ++idraw){
+        freqs[drawer()]++;
+    }
+    ASSERT_TRUE(consts::floats_nearly_equal(0.2/(freqs[0]/(double)ndraw), 1.0, 1e-3));
+    ASSERT_TRUE(consts::floats_nearly_equal(0.4/(freqs[1]/(double)ndraw), 1.0, 1e-3));
+    ASSERT_TRUE(consts::floats_nearly_equal(0.1/(freqs[2]/(double)ndraw), 1.0, 1e-3));
+    ASSERT_TRUE(consts::floats_nearly_equal(0.3/(freqs[3]/(double)ndraw), 1.0, 1e-3));
+}
