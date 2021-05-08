758f02016184810838550f8d4b5ef4f88f59b186
Robert John Anderson <robert.anderson@kcl.ac.uk>
2021-05-08 20:51:39 +0100

    minor

--- src/core/basis/CiSpaces.cpp
+++ src/core/basis/CiSpaces.cpp
@@ -3,3 +3,39 @@
 //
 
 #include "CiSpaces.h"
+
+ci_gen::Base::Base(size_t nsite, size_t nelec) : m_nsite(nsite), m_nelec(nelec){}
+
+ci_gen::NoSym::NoSym(size_t nsite, size_t nelec) :
+        Base(nsite, nelec), m_foreach(2*nsite, nelec) {}
+
+void ci_gen::NoSym::operator()(Row &row, fields::FermionOnv &onv) {
+    ASSERT(onv.belongs_to_row(&row));
+    row.m_table->clear();
+    auto body = [&](const defs::inds& inds){
+        row.push_back_jump();
+        onv = inds;
+        row.m_table->post_insert(row.m_i);
+    };
+    m_foreach(body);
+}
+
+ci_gen::SpinSym::SpinSym(size_t nsite, size_t nelec, int spin) :
+        Base(nsite, nelec),
+        m_foreach_alpha(nsite, ci_utils::nalpha(nelec, spin)),
+        m_foreach_beta(nsite, ci_utils::nbeta(nelec, spin)){}
+
+void ci_gen::SpinSym::operator()(Row &row, fields::FermionOnv &onv) {
+    ASSERT(onv.belongs_to_row(&row));
+    row.m_table->clear();
+
+    auto alpha_body = [&](const defs::inds& alpha_inds){
+        auto beta_body = [&](const defs::inds& beta_inds){
+            row.push_back_jump();
+            onv.set(alpha_inds, beta_inds);
+            row.m_table->post_insert(row.m_i);
+        };
+        m_foreach_beta(beta_body);
+    };
+    m_foreach_alpha(alpha_body);
+}
--- src/core/basis/CiSpaces.h
+++ src/core/basis/CiSpaces.h
@@ -13,48 +13,22 @@ namespace ci_gen {
 
     struct Base {
         const size_t m_nsite, m_nelec;
-        Base(size_t nsite, size_t nelec) : m_nsite(nsite), m_nelec(nelec){}
+        Base(size_t nsite, size_t nelec);
     };
 
     struct NoSym : Base {
         foreach::rtnd::Ordered<> m_foreach;
-        NoSym(size_t nsite, size_t nelec) :
-        Base(nsite, nelec), m_foreach(2*nsite, nelec) {}
+        NoSym(size_t nsite, size_t nelec);
 
-        void operator()(Row& row, fields::FermionOnv& onv) {
-            ASSERT(onv.belongs_to_row(&row));
-            row.m_table->clear();
-            auto body = [&](const defs::inds& inds){
-                row.push_back_jump();
-                onv = inds;
-                row.m_table->post_insert(row.m_i);
-            };
-            m_foreach(body);
-        }
+        void operator()(Row& row, fields::FermionOnv& onv);
     };
 
     struct SpinSym : Base {
         foreach::rtnd::Ordered<> m_foreach_alpha;
         foreach::rtnd::Ordered<> m_foreach_beta;
-        SpinSym(size_t nsite, size_t nelec, int spin) :
-                Base(nsite, nelec),
-                m_foreach_alpha(nsite, ci_utils::nalpha(nelec, spin)),
-                m_foreach_beta(nsite, ci_utils::nbeta(nelec, spin)){}
+        SpinSym(size_t nsite, size_t nelec, int spin);
 
-        void operator()(Row& row, fields::FermionOnv& onv) {
-            ASSERT(onv.belongs_to_row(&row));
-            row.m_table->clear();
-
-            auto alpha_body = [&](const defs::inds& alpha_inds){
-                auto beta_body = [&](const defs::inds& beta_inds){
-                    row.push_back_jump();
-                    onv.set(alpha_inds, beta_inds);
-                    row.m_table->post_insert(row.m_i);
-                };
-                m_foreach_beta(beta_body);
-            };
-            m_foreach_alpha(alpha_body);
-        }
+        void operator()(Row& row, fields::FermionOnv& onv);
     };
 };
 
