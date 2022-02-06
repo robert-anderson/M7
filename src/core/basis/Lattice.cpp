62a11deaed6a5d77dd56b57d6557661896a01453
Robert Anderson <robert.anderson@kcl.ac.uk>
2022-02-06 01:10:32 +0100

    putting Lattice class implementations into cpp file

--- src/core/basis/Lattice.cpp
+++ src/core/basis/Lattice.cpp
@@ -3,3 +3,55 @@
 //
 
 #include "Lattice.h"
+
+Lattice::Topology Lattice::topology(const std::string &str) {
+    if (str=="ortho") return Ortho;
+    if (str=="orthogonal") return Ortho;
+    ABORT("invalid name given for lattice topology");
+    return NullTopology;
+}
+
+std::string Lattice::topo_string(Lattice::Topology topo) {
+    switch (topo) {
+        case Ortho: return "orthogonal";
+        case NullTopology: return "null";
+    }
+    return {};
+}
+
+const size_t &Lattice::nsite() const {
+    return m_dense.nrow();
+}
+
+std::string Lattice::info() const {
+    return log::format("{} lattice with site shape: {}, boundary conds {}",
+                       topo_string(m_spec.m_topo), utils::to_string(m_spec.m_format.m_shape),
+                       utils::to_string(m_spec.m_bcs));
+}
+
+void Lattice::add(size_t irow, const defs::inds &icols, const std::vector<int> &coeffs) {
+    REQUIRE_TRUE(m_sparse.empty(irow), "row already added");
+    REQUIRE_EQ(icols.size(), coeffs.size(), "unequal lengths of column indices and values");
+    auto ncol = icols.size();
+    if ((m_unique_nconn_product/ncol)*ncol != m_unique_nconn_product) m_unique_nconn_product*=ncol;
+    for (size_t iicol=0ul; iicol<ncol; ++iicol) {
+        auto icol = icols[iicol];
+        auto coeff = coeffs[iicol];
+        m_sparse.add(irow, {icol, coeff});
+        m_dense(irow, icol) = coeff;
+    }
+}
+
+Lattice::Lattice(size_t nsite, Lattice::Spec spec) :
+        m_dense(nsite), m_spec(std::move(spec)) {
+    m_sparse.resize(nsite);
+}
+
+Lattice::Spec::Spec(Lattice::Topology topo, const defs::inds &shape, std::vector<int> bcs) :
+        m_topo(topo), m_format(shape), m_bcs(std::move(bcs)){
+    REQUIRE_EQ(m_format.m_nind, m_bcs.size(),
+               "site shape and boundary conds should be the same length");
+}
+
+Lattice::Spec::Spec(const std::string &topo, const defs::inds &shape, std::vector<int> bcs) :
+        Spec(topology(topo), shape, std::move(bcs)){}
--- src/core/basis/Lattice.h
+++ src/core/basis/Lattice.h
@@ -35,33 +35,17 @@ struct Lattice {
     enum Topology {Ortho, NullTopology};
 
 private:
-    static Topology topology(const std::string& str) {
-        if (str=="ortho") return Ortho;
-        if (str=="orthogonal") return Ortho;
-        ABORT("invalid name given for lattice topology");
-        return NullTopology;
-    }
+    static Topology topology(const std::string& str);
 
-    static std::string topo_string(Topology topo) {
-        switch (topo) {
-            case Ortho: return "orthogonal";
-            case NullTopology: return "null";
-        }
-        return {};
-    }
+    static std::string topo_string(Topology topo);
 
 public:
     struct Spec {
         Topology m_topo;
         NdFormatD m_format;
         std::vector<int> m_bcs;
-        Spec(Topology topo, const defs::inds& shape, std::vector<int> bcs):
-            m_topo(topo), m_format(shape), m_bcs(std::move(bcs)){
-            REQUIRE_EQ(m_format.m_nind, m_bcs.size(),
-                       "site shape and boundary conds should be the same length");
-        }
-        Spec(const std::string& topo, const defs::inds& shape, std::vector<int> bcs):
-            Spec(topology(topo), shape, std::move(bcs)){}
+        Spec(Topology topo, const defs::inds& shape, std::vector<int> bcs);
+        Spec(const std::string& topo, const defs::inds& shape, std::vector<int> bcs);
     };
     /**
      * complete specification for all lattice subtypes
@@ -80,34 +64,13 @@ public:
      */
     size_t m_unique_nconn_product = 1ul;
 
+    const size_t& nsite() const;
 
-    const size_t& nsite() const {
-        return m_dense.nrow();
-    }
+    std::string info() const;
 
-    std::string info() const {
-        return log::format("{} lattice with site shape: {}, boundary conds {}",
-                 topo_string(m_spec.m_topo), utils::to_string(m_spec.m_format.m_shape),
-                 utils::to_string(m_spec.m_bcs));
-    }
+    void add(size_t irow, const defs::inds& icols, const std::vector<int>& coeffs);
 
-    void add(size_t irow, const defs::inds& icols, const std::vector<int>& coeffs) {
-        REQUIRE_TRUE(m_sparse.empty(irow), "row already added");
-        REQUIRE_EQ(icols.size(), coeffs.size(), "unequal lengths of column indices and values");
-        auto ncol = icols.size();
-        if ((m_unique_nconn_product/ncol)*ncol != m_unique_nconn_product) m_unique_nconn_product*=ncol;
-        for (size_t iicol=0ul; iicol<ncol; ++iicol) {
-            auto icol = icols[iicol];
-            auto coeff = coeffs[iicol];
-            m_sparse.add(irow, {icol, coeff});
-            m_dense(irow, icol) = coeff;
-        }
-    }
-
-    Lattice(size_t nsite, Spec spec):
-        m_dense(nsite), m_spec(std::move(spec)) {
-        m_sparse.resize(nsite);
-    }
+    Lattice(size_t nsite, Spec spec);
 
 };
 
