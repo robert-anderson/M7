c10f0736dace661329c52d5caeb5643060c2a844
Robert Anderson <robert.anderson@kcl.ac.uk>
2022-03-09 01:35:13 +0100

    adding class to encapsulate N-dimensional planewave basis

---
+++ src/core/basis/Planewaves.cpp
@@ -0,0 +1,94 @@
+//
+// Created by rja on 08/03/2022.
+//
+
+#include "Planewaves.h"
+
+defs::inds Planewaves::make_momentum_shape(const defs::inds &wave_shape) {
+    defs::inds shape;
+    for (auto extent: wave_shape) shape.push_back(2*extent+1);
+    return shape;
+}
+
+std::vector<std::vector<int>> Planewaves::make_momvecs(const defs::inds &wave_shape) {
+    std::vector<std::vector<int>> momvecs;
+    auto momentum_shape = make_momentum_shape(wave_shape);
+    momvecs.reserve(NdFormatD(momentum_shape).m_nelement);
+    auto fn = [&wave_shape, &momvecs](const defs::inds& inds, size_t iiter){
+        momvecs.emplace_back();
+        for (size_t idim=0ul; idim < wave_shape.size(); ++idim){
+            momvecs.back().push_back(int(inds[idim]) - wave_shape[idim]);
+        }
+    };
+    foreach_virtual::rtnd::lambda::Unrestricted foreach(fn, momentum_shape);
+    foreach.loop();
+    DEBUG_ASSERT_EQ(momvecs.size(), momvecs.capacity(), "incorrect number of momentum vectors generated");
+    return momvecs;
+}
+
+Planewaves::Planewaves(const defs::inds& wave_shape) :
+    m_wave_format(wave_shape), m_momentum_format(make_momentum_shape(wave_shape)),
+    m_nbasis(m_momentum_format.m_nelement), m_ndim(m_momentum_format.m_nind),
+    m_momvecs(make_momvecs(wave_shape)){}
+
+Planewaves::Planewaves(size_t ndim, size_t nwave) : Planewaves(defs::inds(ndim, nwave)){}
+
+const std::vector<int> &Planewaves::operator[](const size_t &i) const {
+    DEBUG_ASSERT_LT(i, m_nbasis, "basis function index OOB");
+    return m_momvecs[i];
+}
+
+bool Planewaves::conserving(const size_t &icre, const size_t &iann) const {
+    auto& cre_mom = (*this)[icre];
+    auto& ann_mom = (*this)[iann];
+    for (size_t idim=0ul; idim<m_ndim; ++idim) if (cre_mom[idim]!=ann_mom[idim]) return false;
+    return true;
+}
+
+bool Planewaves::conserving(const size_t &icre1, const size_t &icre2, const size_t &iann1, const size_t &iann2) const {
+    auto& cre_mom1 = (*this)[icre1];
+    auto& cre_mom2 = (*this)[icre2];
+    auto& ann_mom1 = (*this)[iann1];
+    auto& ann_mom2 = (*this)[iann2];
+    for (size_t idim=0ul; idim<m_ndim; ++idim){
+        if (cre_mom1[idim]+cre_mom2[idim]!=ann_mom1[idim]+ann_mom2[idim]) return false;
+    }
+    return true;
+}
+
+int Planewaves::diff(const size_t &idim, const size_t &i, const size_t &j) const {
+    DEBUG_ASSERT_LT(idim, m_ndim, "dimension index OOB");
+    auto& imom = (*this)[i];
+    auto& jmom = (*this)[j];
+    return imom[idim]-jmom[idim];
+}
+
+void Planewaves::diff(std::vector<int> &diff, const size_t &i, const size_t &j) const {
+    auto& imom = (*this)[i];
+    auto& jmom = (*this)[j];
+    diff.clear();
+    for (size_t idim=0ul; idim<m_ndim; ++idim) diff.push_back(imom[idim]-jmom[idim]);
+}
+
+size_t Planewaves::diff(const size_t &i, const size_t &j) const {
+    diff(m_momvec_work, i, j);
+    return encode(m_momvec_work);
+}
+
+size_t Planewaves::encode(const std::vector<int> &momvec) const {
+    m_inds_work.clear();
+    for (size_t idim=0ul; idim<m_ndim; ++idim)
+        m_inds_work.push_back(momvec[idim]+m_wave_format.m_shape[idim]);
+    return m_momentum_format.flatten(m_inds_work);
+}
+
+const size_t &Planewaves::size() const {
+    return m_nbasis;
+}
+
+int Planewaves::kinetic_energy(const size_t &i) const {
+    auto& momvec = (*this)[i];
+    int tot = 0;
+    for (auto& mom: momvec) tot+=mom*mom;
+    return tot;
+}
---
+++ test/core/basis/Planewaves.cpp
@@ -0,0 +1,28 @@
+//
+// Created by rja on 09/03/2022.
+//
+
+#include "gtest/gtest.h"
+#include "src/core/basis/Planewaves.h"
+
+TEST(Planewaves, HeterogeneousExtents) {
+    defs::inds shape = {3, 5, 2};
+    Planewaves planewaves(shape);
+    size_t chk_size = 1;
+    for (auto &extent: shape) chk_size*=(2*extent+1);
+    ASSERT_EQ(planewaves.size(), chk_size);
+    for (size_t i=0ul; i<planewaves.size(); ++i){
+        auto& momvec = planewaves[i];
+        ASSERT_EQ(planewaves.encode(momvec), i);
+    }
+}
+
+TEST(Planewaves, HomogeneousExtents) {
+    const size_t ndim=3, nwave = 4;
+    Planewaves planewaves(ndim, nwave);
+    ASSERT_EQ(planewaves.size(), std::pow(2*nwave+1, ndim));
+    for (size_t i=0ul; i<planewaves.size(); ++i){
+        auto& momvec = planewaves[i];
+        ASSERT_EQ(planewaves.encode(momvec), i);
+    }
+}
