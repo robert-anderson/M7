0af3b86bf8c35a7b17c8634d0526b3585dcd669f
Robert Anderson <robert.anderson@kcl.ac.uk>
2022-03-22 16:44:52 +0100

    removing obsolete model-detection code
    models should be specified through subclasses of the FrmHam, LadderHam, and BosHam polymorphic base classes

--- src/M7_lib/hamiltonian/HamiltonianData.cpp
+++ src/M7_lib/hamiltonian/HamiltonianData.cpp
@@ -30,52 +30,6 @@ bool ham_data::TermContribs::is_nonzero(size_t exsig) const {
     return m_exsig_nonzero[i];
 }
 
-bool ham_data::FrmModelAttributes::is_hubbard_1d() const {
-    return m_on_site_only_doubles && m_nn_only_singles;
-}
-
-bool ham_data::FrmModelAttributes::is_hubbard_1d_pbc() const {
-    return m_on_site_only_doubles && (m_nnp_only_singles && !m_nn_only_singles);
-}
-
-size_t ham_data::FrmModelAttributes::iorb_to_isite(size_t iorb, size_t nsite) {
-    return iorb < nsite ? iorb : iorb + nsite;
-}
-
-bool ham_data::FrmModelAttributes::nearest_neighbors(size_t nsite, size_t iorb, size_t jorb,
-                                                     bool periodic) {
-    auto isite = iorb_to_isite(iorb, nsite);
-    auto jsite = iorb_to_isite(jorb, nsite);
-    if (isite + 1 == jsite || jsite + 1 == isite) return true;
-    if (periodic) {
-        return (isite == 0 && jsite == nsite - 1) || (isite == nsite - 1 && jsite == 0);
-    }
-    return false;
-}
-
-bool
-ham_data::FrmModelAttributes::on_site(size_t nsite, size_t iorb, size_t jorb, size_t korb,
-                                      size_t lorb) const {
-    auto isite = iorb_to_isite(iorb, nsite);
-    auto jsite = iorb_to_isite(jorb, nsite);
-    auto ksite = iorb_to_isite(korb, nsite);
-    auto lsite = iorb_to_isite(lorb, nsite);
-    return isite == jsite && jsite == ksite && ksite == lsite;
-}
-
-void ham_data::FrmModelAttributes::nonzero(size_t nsite, size_t i, size_t j) {
-    // non-diagonal 1body term is not disqualifying for "hubbard-like" Hamiltonian designation
-    if (i != j){
-        if (!nearest_neighbors(nsite, i, j, false)) m_nn_only_singles = false;
-        if (!nearest_neighbors(nsite, i, j, true)) m_nnp_only_singles = false;
-    }
-}
-
-void ham_data::FrmModelAttributes::nonzero(size_t nsite, size_t i, size_t j, size_t k,
-                                           size_t l) {
-    if (!(i == j && j == k && k == l)) m_on_site_only_doubles = false;
-}
-
 bool ham_data::KramersAttributes::conserving() const {
     return m_conserving_singles && m_conserving_double;
 }
--- src/M7_lib/hamiltonian/HamiltonianData.h
+++ src/M7_lib/hamiltonian/HamiltonianData.h
@@ -33,51 +33,6 @@ namespace ham_data {
 
     };
 
-    struct FrmModelAttributes {
-
-        bool m_nn_only_singles = true;
-        /**
-         * same as above, but with (1D) periodic boundary conditions
-         */
-        bool m_nnp_only_singles = true;
-
-        /**
-         * are (2, 2)-rank contribs due to (0, 0)-excitations nonzero only when all operator indices refer to the same site?
-         * assume this is the case unless counterexample found in loop over nonzero elements
-         */
-        bool m_on_site_only_doubles = true;
-
-        bool is_hubbard_1d() const;
-
-        bool is_hubbard_1d_pbc() const;
-        /**
-         * @param iorb
-         *  orbital index (site if integrals spin resolved, else spin oribtal index)
-         * @return
-         *  site index
-         */
-        static size_t iorb_to_isite(size_t iorb, size_t nsite);
-
-        /**
-         * @param iorb
-         * @param jorb
-         * @param periodic
-         *  if true, lowest and highest site indices count as neighbors.
-         * @return
-         * true if orbitals are nearest neighbors
-         */
-        static bool nearest_neighbors(size_t nsite, size_t iorb, size_t jorb, bool periodic);
-
-        bool on_site(size_t nsite, size_t iorb, size_t jorb, size_t korb, size_t lorb) const;
-
-        void nonzero(size_t  nsite, size_t  i, size_t  j);
-
-        /**
-         * non-zero integral reached with chemical notation (ij|kl)
-         */
-        void nonzero(size_t  nsite, size_t  i, size_t  j, size_t  k, size_t  l);
-    };
-
     struct KramersAttributes {
         bool m_conserving_singles = true;
         bool m_conserving_double = true;
