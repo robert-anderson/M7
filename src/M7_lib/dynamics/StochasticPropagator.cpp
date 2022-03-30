ca02111a8f2617a5822c7ad835fa0b72bbd7d318
Robert Anderson <robert.anderson@kcl.ac.uk>
2022-03-30 20:03:04 +0200

    fixing calculation of p_succeed_at_least_once in stochastic bilinear sampling
    this is required since the introduction of the consolidated annihilation algorithm

--- src/M7_lib/dynamics/StochasticPropagator.cpp
+++ src/M7_lib/dynamics/StochasticPropagator.cpp
@@ -27,22 +27,26 @@ StochasticPropagator::StochasticPropagator(const Hamiltonian &ham, const fciqmc_
 void StochasticPropagator::off_diagonal(Wavefunction &wf, const size_t &ipart) {
     const auto &row = wf.m_store.m_row;
     const defs::wf_t &weight = row.m_weight[ipart];
-    double rdm_factor = 1.0;
+    /*
+     * for bilinear estimators based on the consolidated annihilation of spawned contributions
+     */
+    defs::prob_t p_succeed_at_least_once = 1.0;
 
-    DEBUG_ASSERT_NE(weight, 0.0, "should not attempt offdiagonal propagation from zero weight");
+    DEBUG_ASSERT_NE(weight, 0.0, "should not attempt off-diagonal propagation from zero weight");
     DEBUG_ASSERT_TRUE(consts::imag(weight) == 0.0 || m_ham.complex_valued(),
                       "real-valued hamiltonian should never result in non-zero imaginary walker component")
     const auto &src_mbf = row.m_mbf;
     bool flag_initiator = row.m_initiator.get(ipart);
     bool flag_deterministic = row.m_deterministic.get(wf.iroot_part(ipart));
 
-    size_t nattempt = get_nattempt(weight);
+    const defs::wf_comp_t abs_weight = std::abs(weight);
+    defs::prob_t prob_nattempt_floor, prob_gen, prob_thresh_accept;
+    defs::ham_t helem;
+
+    auto nattempt = size_t(m_prng.stochastic_round(abs_weight, 1.0, prob_nattempt_floor));
     if (!nattempt) return;
 
     m_excit_gens.clear_cached_orbs();
-
-    defs::prob_t prob;
-    defs::ham_t helem;
 
     auto &conn = m_conn[src_mbf];
     auto &dst_mbf = m_dst[src_mbf];
@@ -50,26 +54,59 @@ void StochasticPropagator::off_diagonal(Wavefunction &wf, const size_t &ipart) {
 
         conn.clear();
         auto iex = m_excit_gens.draw_iex();
-        if (!m_excit_gens.draw(iex, src_mbf, prob, helem, conn)) {
+        if (!m_excit_gens.draw(iex, src_mbf, prob_gen, helem, conn)) {
             // null excitation generated
             continue;
         }
-        m_mag_log.log(iex, helem, prob);
-        prob *= m_excit_gens.get_prob(iex);
+        m_mag_log.log(iex, helem, prob_gen);
+        prob_gen *= m_excit_gens.get_prob(iex);
 
         conn.apply(src_mbf, dst_mbf);
-        auto delta = -tau() * phase(weight) * helem / prob;
+        auto delta = -tau() * phase(weight) * helem / prob_gen;
         if (consts::nearly_zero(delta, 1e-14)) continue;
         imp_samp_delta(delta, src_mbf, dst_mbf, row.m_hdiag);
-        delta = m_prng.stochastic_threshold(delta, m_opts.m_propagator.m_min_spawn_mag);
-        if (consts::nearly_zero(delta, 1e-14)) continue;
+        /*
+         * the stochastically-realized spawned contribution is equal to delta if delta is not lower in magnitude than
+         * the minimum magnitude, otherwise it is stochastically rounded with respect to that magnitude
+         */
+        auto thresh_delta = m_prng.stochastic_threshold(
+                delta, m_opts.m_propagator.m_min_spawn_mag, prob_thresh_accept);
+        if (thresh_delta==0.0) continue;
 
         if (wf.recv().m_row.m_send_parents) {
-            // reweight by probability that this connection was sampled a non-zero number of times
-            rdm_factor = 1.0 / (1.0 - std::pow(1 - prob, nattempt));
+            /*
+             * we require the parent weights to be sent along with the spawned contributions, so the instantaneous
+             * weight must be "unbiased" by the probability that this connection was sampled a non-zero number of times.
+             * there are 3 stochastic processes involved:
+             *  1. number of attempts is either the *floor* or ceiling of the instantaneous weight (prob_nattempt_floor)
+             *  2. generate a connected MBF (prob_gen)
+             *  3. *accept* or reject based on the magnitude of the candidate walker (prob_thresh_accept)
+             */
+            defs::prob_t p_fail_one_attempt, p_fail_all_attempts;
+            /*
+             * failure to generate this connection at one attempt could be due to either
+             *  1. generating some other connection via the excitation generator
+             */
+            p_fail_one_attempt = (1.0 - prob_gen);
+            /*
+             * 2. generating this connection only for it to be rejected by the stochastic threshold operation
+             */
+            p_fail_one_attempt += prob_gen * (1.0 - prob_thresh_accept);
+            /*
+             * now, failure to generate this connection at all attempts could be due to either
+             *  1. deciding to draw the minimum number of attempts, only to fail on each attempt
+             */
+            auto floor = std::floor(abs_weight);
+            p_fail_all_attempts = prob_nattempt_floor * std::pow(p_fail_one_attempt, floor);
+            /*
+             *  2. deciding to draw the maximum number of attempts, only to fail on each attempt
+             */
+            p_fail_all_attempts += (1.0 - prob_nattempt_floor) * std::pow(p_fail_one_attempt, floor+1.0);
+
+            p_succeed_at_least_once = 1.0 - p_fail_all_attempts;
         }
-        wf.add_spawn(dst_mbf, delta, flag_initiator, flag_deterministic,
-                     ipart, src_mbf, rdm_factor * weight);
+        wf.add_spawn(dst_mbf, thresh_delta, flag_initiator, flag_deterministic,
+                     ipart, src_mbf, weight / p_succeed_at_least_once);
     }
 }
 
