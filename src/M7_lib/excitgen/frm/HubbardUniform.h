97deac716d91a6c2bd11b0e259fb2d11334584f0
robert-anderson <robert.anderson@kcl.ac.uk>
2022-06-06 20:29:02 +0200

    setting the probability method of the HubbardUniform excit gen

--- src/M7_lib/excitgen/frm/HubbardUniform.h
+++ src/M7_lib/excitgen/frm/HubbardUniform.h
@@ -8,6 +8,10 @@
 #include "FrmExcitGen.h"
 #include "M7_lib/hamiltonian/frm/HubbardFrmHam.h"
 
+/**
+ * excitation generator for the N-dimensional Hubbard model which does not constrain the creation operator drawn based
+ * on the occupation
+ */
 struct HubbardUniform : FrmLatticeExcitGen {
 
     HubbardUniform(const FrmHam& h, PRNG& prng);
@@ -15,6 +19,8 @@ struct HubbardUniform : FrmLatticeExcitGen {
     virtual ~HubbardUniform(){}
 
     bool draw_frm(const size_t &exsig, const field::FrmOnv &src, defs::prob_t &prob, conn::FrmOnv &conn) override;
+
+    defs::prob_t prob_frm(const field::FrmOnv &src, const conn::FrmOnv &conn) const override;
 
     size_t approx_nconn(size_t exsig, sys::Particles particles) const override;
 
