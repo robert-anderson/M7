62b156915a97a7b665d17fd523ad2a7d7c9770b3
jphaupt <jphaupt@protonmail.com>
2022-06-15 10:10:34 +0200

    Higher precision dense frm ham test

--- test/test_core/linalg/DenseTcFermionHamiltonian.cpp
+++ test/test_core/linalg/DenseTcFermionHamiltonian.cpp
@@ -23,14 +23,15 @@ TEST(DenseTcFermionHamiltonian, TcBe631G) {
     TcFrmHam ham_src({"FCIDUMP"}, false);
     Hamiltonian gham(&ham_src);
     DenseHamiltonian ham(gham);
-    // non-symmetric real matrices have complex eigenvalues
+    // non-symmetric real matrices have (in general) complex eigenvalues
     std::vector<std::complex<defs::ham_t>> evals;
     dense::diag(ham, evals); // dense diagonalisation
     // unlike hermitian case, we need to sort the eigenvalue array by the real part (lowest first)
     sort_utils::inplace(evals, false, false);
+    // std::cout.precision(17);
     // std::cout << evals[0] << std::endl; // print ground state
     // compare to NECI calculation
-    ASSERT_NEARLY_EQ(evals[0].real(), -14.6663, 1e-4);
+    ASSERT_NEARLY_EQ(evals[0].real(), -14.666331930789127, 1e-8);
     // check that all eigenvalues have zero imaginary component
     for (unsigned int i=0; i < evals.size(); ++i) {
         ASSERT_NEARLY_EQ(evals[i].imag(), 0.0, 1e-10);
