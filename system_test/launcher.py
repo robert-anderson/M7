9cb09494869c67fe7dce04c7697af6c0647ad3e1
robert-anderson <robert.anderson@kcl.ac.uk>
2022-08-18 23:10:21 +0200

    updating system testing scripts

--- system_test/launcher.py
+++ system_test/launcher.py
@@ -4,7 +4,7 @@ runs all tests under the given directory in parallel
 import tempfile, shutil, sys, time
 '''
 can't actually use this module directly since workload is determined by the number of MPI ranks
-no the number of python processes
+not the number of python processes
 '''
 import multiprocessing
 from pathlib import Path
@@ -15,6 +15,9 @@ import argparse
 parser = argparse.ArgumentParser(description='Run M7 system tests in parallel')
 parser.add_argument('mode', type=str, help='test or redefine benchmarks')
 parser.add_argument('m7_exe', type=str, help='path to M7 binary')
+parser.add_argument('asset_dir', type=str, help='path to asset directory')
+parser.add_argument('--root_dir', type=str, help='path to directory below which all tests should be performed',
+        default='.')
 parser.add_argument('--mpirun', type=str, help='path to mpirun executable', default='mpirun')
 parser.add_argument('--nslot', type=int, help='total number of slots to make available for M7 jobs',
         default=multiprocessing.cpu_count())
@@ -23,9 +26,17 @@ parser.add_argument('--update_period', type=float, help='interval between queue 
 args = parser.parse_args()
 assert args.mode in ('benchmark', 'test')
 
+'''
+set a temporary directory to act as a job queue
+'''
 queue_dir = Path(tempfile.mkdtemp())
 print(f'reading job queue from {queue_dir}')
 
+'''
+jobs currently running.
+keys are the uuid the jobs generate for themselves
+values are the number of slots allocated to the job
+'''
 running_jobs = {}
 
 def nslot_avail():
@@ -34,8 +45,13 @@ def nslot_avail():
     return navail
 
 def update():
+    '''
+    queued jobs will touch a file in the queue directory named with the uuid (name)
+    followed by a '.', then the number of slots requested
+    '''
     for job in queue_dir.glob('*'):
         if '.' in job.name:
+            # this is a request by a job for resources
             try: 
                 name, nslot = job.name.split('.')
                 nslot = int(nslot)
@@ -43,35 +59,53 @@ def update():
                 # invalid filename
                 continue
             if nslot <= nslot_avail():
+                # remove the request
                 job.unlink()
                 running_jobs[name] = nslot
         else:
+            # this is indicating that the job is completed
             if job.name in running_jobs:
                 del running_jobs[job.name]
 
 sys_test_dir = Path().absolute()
 
 tests = []
+# Popen process instances
 procs = []
-for f in sorted(Path().rglob('test.py')):
+'''
+find all directories under root_dir which contain a test.py script
+'''
+for f in sorted(Path(args.root_dir).rglob('test.py')):
     tests.append({'path': f.parent})
     cmd = f'env PYTHONPATH={sys_test_dir} python {f.name} '
-    cmd += f'{args.mode} {args.m7_exe} --mpirun={args.mpirun} --queue_dir={queue_dir}'
+    cmd += f'{args.mode} {args.m7_exe} {args.asset_dir} --mpirun={args.mpirun} --queue_dir={queue_dir}'
     procs.append(Popen(cmd, shell=True, cwd=f.parent, stdout=PIPE, stderr=PIPE))
 
+'''
+poll() method of process returns None if it is not complete
+keep polling all processes until there are not any returning None
+'''
 while any(proc.poll() is None for proc in procs):
     time.sleep(args.update_period)
     update()
 
+'''
+all processes are completed, call communicate method to get stdout and stderr outputs
+'''
 for iproc, proc in enumerate(procs):
     out, err = proc.communicate()
-    tests[iproc]['out'], tests[iproc]['err'] = str(out, 'utf8'), str(err, 'utf8')
+    test = tests[iproc]
+    test['out'], test['err'], test['code'] = str(out, 'utf8'), str(err, 'utf8'), proc.returncode
 
+'''
+based on stderr and return code, decide the test outcome
+'''
 def result(test):
-    if 'FAILURE' in test['err']: return 'FAILED'
+    if 'FAILURE' in test['err']: return test['err'].split('Exception: ')[1]
     elif 'SKIP' in test['err']: return 'SKIPPED'
-    return 'PASSED'
-
+    elif 'No benchmark defined' in test['err']: return 'BENCHMARK UNDEFINED'
+    elif test['code']: return 'TEST ERROR:'+test['err']
+    return 'NEW BENCHMARK DEFINED' if (args.mode=='benchmark') else 'PASSED'
 
 for test in tests:
     print('{:<50} {}'.format(str(test['path']), result(test)))
--- system_test/system_test.py
+++ system_test/system_test.py
@@ -10,6 +10,7 @@ import time, argparse
 parser = argparse.ArgumentParser(description='Run this M7 system test')
 parser.add_argument('mode', type=str, help='test or redefine benchmarks')
 parser.add_argument('m7_exe', type=str, help='path to M7 binary')
+parser.add_argument('asset_dir', type=str, help='path to asset directory')
 parser.add_argument('--mpirun', type=str, help='path to mpirun executable', default=None)
 parser.add_argument('--queue_dir', type=str, help='path queue for parallel testing', default=None)
 args = parser.parse_args()
@@ -27,7 +28,7 @@ compile_defs = None
 benchmarking = args.mode=='benchmark'
 
 # if testing, make sure we have something to compare against
-if not benchmarking: assert bench_dir.exists()
+if not benchmarking: assert bench_dir.exists(), 'No benchmark defined'
 
 request_delay = 0.2
 
@@ -45,6 +46,9 @@ for line in shell(args.m7_exe)[0].split('\n'):
 # validate given paths
 assert compile_defs is not None, "invalid M7 binary"
 if args.mpirun is not None: assert 'mpi' in shell(f'{args.mpirun} --version')[0].lower(), 'invalid mpirun path'
+
+asset_dir = Path(args.asset_dir)
+assert asset_dir.exists()
 
 class StatsFile:
     columns = []
@@ -69,20 +73,22 @@ class TestInstance:
     irun_last_stats_update = 0
     run_stats = None
     bench_stats = None
+    redefine_benchmark = True
 
     def stats(self, fname='M7.stats'):
-        if benchmarking or len(self.error_msg): return
+        if len(self.error_msg): return
         if self.irun_last_stats_update != self.irun:
-            self.run_stats = StatsFile(run_dir/fname)
-            self.bench_stats = StatsFile(bench_dir/fname)
             self.irun_last_stats_update = self.irun
-            if self.run_stats.columns != self.bench_stats.columns:
-                self.error_msg = "incompatible column names"
+            self.run_stats = StatsFile(run_dir/fname)
+            if not benchmarking: 
+                self.bench_stats = StatsFile(bench_dir/fname)
+                if self.run_stats.columns != self.bench_stats.columns:
+                    self.error_msg = "incompatible column names"
         return self.run_stats, self.bench_stats
 
     def __del__(self):
         print(self.error_msg)
-        if not benchmarking: return
+        if not benchmarking or not self.redefine_benchmark: return
         shutil.rmtree(bench_dir, ignore_errors=True)
         shutil.move(run_dir, bench_dir)
 
@@ -90,47 +96,85 @@ class TestInstance:
 # when this goes out of scope, the benchmark will be adjusted if requested
 instance = TestInstance()
 def fail(msg):
+    instance.redefine_benchmark = False
     raise Exception(f'FAILURE: {msg}')
 
 def skip():
+    instance.redefine_benchmark = False
     raise Exception('SKIP')
 
 def stats_columns(col_name, fname='M7.stats'):
     stats = instance.stats(fname)
     column = stats[0].lookup_column(col_name)
     assert column is not None
-    return stats[0].data[:,column[0]], stats[1].data[:,column[0]]
+    if not benchmarking:
+        return stats[0].data[:,column[0]], stats[1].data[:,column[0]]
+    else:
+        return stats[0].data[:,column[0]], None
 
-def bring(fname, src_dir=None):
-    src = Path()/fname if src_dir is None else Path(src_dir)/fname
-    dst = run_dir/fname
-    assert src.exists()
+def is_vector(obj):
+    return isinstance(obj, tuple) or isinstance(obj, list)
+
+def resolve_src_path(path):
+    # first, attempt to resolve path locally
+    src = Path()/path
+    # if this fails, look relative to the asset directory
+    if not src.exists(): src = asset_dir.joinpath(path)
+    assert src.exists(), f'path "{src}" found neither locally nor under assets directory'
+    return src
+
+def make_local_name(asset):
+    # asset can be tuple
+    if is_vector(asset):
+        # return given local name
+        name = asset[1]
+    else:
+        # inherit local name from that of src
+        name = Path(asset).name
+    return Path(run_dir/name)
+
+def bring(asset):
+    src = resolve_src_path(asset)
+    dst = make_local_name(asset)
     assert not dst.exists()
     os.symlink(src.absolute(), dst.absolute())
 
-def run(config='config.yaml', nrank=1, assets=tuple()):
+def run(config='config.yaml', nrank=1, assets=[]):
     assert nrank
     if not (def_dir/config).exists(): config = ''
-    for asset in assets+(config,): bring(asset)
+    # assets can be tuple, where item 1 is the local name to given to the link
+    assets.append(config)
+    for asset in assets: bring(asset)
     cmd = f'{args.m7_exe} {config}' 
     if args.mpirun is not None: cmd = f'{args.mpirun} -n {nrank} '+cmd
     else: assert nrank==1, 'require more than one rank but no mpirun path is given'
 
     if args.queue_dir is not None:
+        # we're using a queue
+        # generate a unique name for the job
         job_name = uuid()
         request_path = Path(args.queue_dir)/f'{job_name}.{nrank}'
+        # when this path comes into existence, the job is completed
         done_path = Path(args.queue_dir)/f'{job_name}'
+        # request resources from the queue by creating a blank file at the request_path
         request_path.touch()
-        while request_path.exists(): time.sleep(request_delay)
+        # the request must wait its turn
+        while request_path.exists(): 
+            # deletion of the request indicates that the queue approves the job to run
+            time.sleep(request_delay)
 
     instance.irun+=1
     shell(cmd, run_dir)
 
+    # let the queue know that this process is done
     if args.queue_dir is not None: done_path.touch()
 
-    if benchmarking:
-        for asset in assets+(config,): os.unlink(run_dir/asset)
-
+    if benchmarking and instance.redefine_benchmark:
+        '''
+        keep benchmarks clean of symlinks, since these are in general directory structure-dependent
+        only unlink assets if this in benchmarking mode, otherwise leave links in place for debugging
+        '''
+        for asset in assets: os.unlink(make_local_name(asset))
 
 def skip_if(compile_def, value):
     assert compile_def in compile_defs
@@ -144,3 +188,30 @@ def check_nw(fname='M7.stats'):
     if benchmarking: return
     run, bench = stats_columns('WF L1 norm', fname)
     if not np.allclose(run, bench): fail('walker trajectories do not agree')
+
+'''
+perform crude removal of serial correlation
+'''
+def block(series, nblock):
+    avgs = np.zeros(nblock)
+    blocklen = len(series)//nblock
+    for i in range(nblock): avgs[i] = np.mean(series[i*blocklen:(i+1)*blocklen])
+    return np.mean(series), np.sqrt(np.var(avgs)/nblock)
+
+def within_error(correct_value, mean, error):
+    return (correct_value > mean - error) and (correct_value <= mean + error)
+
+'''
+check that a stats column is statistically correct (within errorbars)
+'''
+def check_stat_correct(correct_value, column_name, nburnin, nblock=32, fname='M7.stats'):
+    stats, _ = stats_columns(column_name, fname)
+    mean, err = block(stats[nburnin:], nblock)
+    if not within_error(correct_value, mean, err):
+        fail(f'mean of column "{column_name}" ({mean:.6e}) is not correct ({correct_value:.6e}) within error bars (+/-{err:.3e})')
+
+def check_shift_correct(correct_value, nburnin, nblock=32, fname='M7.stats'):
+    check_stat_correct(correct_value, 'Diagonal shift', nburnin, nblock, fname)
+
+def check_proje_correct(correct_value, nburnin, nblock=32, fname='M7.stats'):
+    check_stat_correct(correct_value, 'Reference-projected energy', nburnin, nblock, fname)
