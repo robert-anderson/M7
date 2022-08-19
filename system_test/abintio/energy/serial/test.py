ec9df614c312ce2577346bc3b6bc83b50f7ded62
Robert Anderson <robert.anderson@kcl.ac.uk>
2022-08-19 13:40:31 +0200

    renaming abinitio system tests

--- system_test/abintio/energy/serial/test.py
+++
@@ -1,8 +0,0 @@
-from system_test import *
-
-skip_unless('many-body basis function', 'fermion (determinant)')
-run(assets=['HF_RDMs/FCIDUMP'])
-exact_e0 =-99.9421389039332
-check_nw()
-check_shift_correct(exact_e0, 10000)
-check_proje_correct(exact_e0, 10000)
