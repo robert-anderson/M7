47e80ca10437e6c9b6c144378b7fdfb33852287e
Robert Anderson <robert.anderson@kcl.ac.uk>
2022-09-29 16:37:04 +0200

    rebenchmarking after the change of trajectories due to #6c85025c2

--- system_test/abinitio/energy/molcas_fcidump/parallel/test.py
+++ system_test/abinitio/energy/molcas_fcidump/parallel/test.py
@@ -1,8 +1,8 @@
 from system_test import *
 
-skip_unless('many-body basis function', 'fermion (determinant)')
+skip_if('many-body basis function', 'boson (permanent)')
 run(nrank=2, assets=[('N2_Molcas/molcas.FciDmp.h5', 'FCIDUMP')])
 exact_e0 = -109.02180323
 check_nw()
-check_shift_correct(exact_e0, 10000)
-check_proje_correct(exact_e0, 10000)
+check_shift_correct(exact_e0, 12500)
+check_proje_correct(exact_e0, 12500)
