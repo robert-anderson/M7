0e5fd5a846694ad5af5e0784d894d90b111606b5
robert-anderson <robert.anderson@kcl.ac.uk>
2022-12-08 19:43:09 +0100

    add parallel HDF5 and ARPACK deps directly to targe. fixes build on Alavi AMD machines

--- cmake/build_or_find_arpack.cmake
+++ cmake/build_or_find_arpack.cmake
@@ -1,28 +1,17 @@
-function(build_arpack target)
-#
-# Download and build static lib target of arpack-ng dependency of arpackpp
-#l
-    file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/arpack-ng/src)
+function(add_arpack target)
+file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/arpack-ng/src)
     ExternalProject_Add(arpack-ng
-            URL https://github.com/robert-anderson/arpack-ng/archive/refs/heads/master.zip
-            URL_MD5 88487b64fab4069f67e2b21c606d4602
-            CMAKE_ARGS -DBUILD_SHARED_LIBS=OFF
-            SOURCE_DIR ${PROJECT_BINARY_DIR}/arpack-ng/src/arpackpp
-            BINARY_DIR ${PROJECT_BINARY_DIR}/arpack-ng/build
-            STAMP_DIR ${PROJECT_BINARY_DIR}/arpack-ng/stamp
-            TMP_DIR ${PROJECT_BINARY_DIR}/arpack-ng/tmp
-            INSTALL_COMMAND ""
+        URL https://github.com/robert-anderson/arpack-ng/archive/refs/heads/master.zip
+        URL_MD5 88487b64fab4069f67e2b21c606d4602
+        CMAKE_ARGS -DBUILD_SHARED_LIBS=OFF
+        SOURCE_DIR ${PROJECT_BINARY_DIR}/arpack-ng/src/arpack-ng
+        BINARY_DIR ${PROJECT_BINARY_DIR}/arpack-ng/build
+        STAMP_DIR ${PROJECT_BINARY_DIR}/arpack-ng/stamp
+        TMP_DIR ${PROJECT_BINARY_DIR}/arpack-ng/tmp
+        INSTALL_COMMAND ""
     )
-
-    add_library(${target} INTERFACE)
-    add_dependencies(${target} arpack-ng)
-    target_include_directories(${target} INTERFACE ${CMAKE_SOURCE_DIR}/external/arpackpp/include)
-    target_link_libraries(${target}
-        INTERFACE
-            ${PROJECT_BINARY_DIR}/arpack-ng/build/libarpack.a)
-    target_link_options(${target}
-        INTERFACE
-            "-lgfortran"
-    )
+add_dependencies(${target} arpack-ng)
+#target_link_options(arpack-ng INTERFACE "-lgfortran")
+target_include_directories(${target} PUBLIC ${CMAKE_SOURCE_DIR}/external/arpackpp/include)
+target_link_libraries(${target} ${PROJECT_BINARY_DIR}/arpack-ng/build/libarpack.a)
 endfunction()
-
--- cmake/build_or_find_hdf5.cmake
+++ cmake/build_or_find_hdf5.cmake
@@ -1,9 +1,9 @@
-function(build_or_find_hdf5 target)
+function(add_parallel_hdf5 target)
     option(BUILD_HDF5 OFF)
     if(BUILD_HDF5)
-#
-# Download and build HDF5
-#
+        #
+        # Download and build HDF5
+        #
         set(OWN_HDF5_ROOT ${PROJECT_BINARY_DIR}/lib/hdf5)
         set(OWN_HDF5_BUILD ${PROJECT_BINARY_DIR}/hdf5_local/)
         set(HDF5_USE_STATIC_LIBRARIES 1)
@@ -12,56 +12,51 @@ function(build_or_find_hdf5 target)
         file(MAKE_DIRECTORY ${OWN_HDF5_ROOT})
         file(MAKE_DIRECTORY ${OWN_HDF5_BUILD})
         ExternalProject_Add(built_hdf5
-                URL
-                    https://support.hdfgroup.org/ftp/HDF5/current/src/hdf5-${HDF5_VERSION}.tar.gz
-                URL_MD5
-                    e115eeb66e944fa7814482415dd21cc4
-                CMAKE_ARGS
-                    -DHDF5_ENABLE_PARALLEL=ON
-                    -DHDF5_BUILD_CPP_LIB=OFF
-                    -DHDF5_BUILD_JAVA=OFF
-                    -DCMAKE_INSTALL_PREFIX=${OWN_HDF5_ROOT}
-                    -DHDF5_ENABLE_THREADSAFE=OFF
-                    -DHDF5_BUILD_TOOLS=OFF
-                    -DHDF5_BUILD_EXAMPLES=OFF
-                    -DBUILD_TESTING=OFF
-                    -DCMAKE_C_FLAGS="-w"
-                SOURCE_DIR
-                    ${OWN_HDF5_BUILD}/src/hdf5-${HDF5_VERSION}
-                BINARY_DIR
-                    ${OWN_HDF5_BUILD}/build
-                STAMP_DIR
-                    ${OWN_HDF5_BUILD}/stamp
-                TMP_DIR
-                    ${OWN_HDF5_BUILD}/tmp
-                INSTALL_DIR
-                    ""
-                INSTALL_COMMAND
-                    make install
+            URL
+                https://support.hdfgroup.org/ftp/HDF5/current/src/hdf5-${HDF5_VERSION}.tar.gz
+            URL_MD5
+                e115eeb66e944fa7814482415dd21cc4
+            CMAKE_ARGS
+                -DHDF5_ENABLE_PARALLEL=ON
+                -DHDF5_BUILD_CPP_LIB=OFF
+                -DHDF5_BUILD_JAVA=OFF
+                -DCMAKE_INSTALL_PREFIX=${OWN_HDF5_ROOT}
+                -DHDF5_ENABLE_THREADSAFE=OFF
+                -DHDF5_BUILD_TOOLS=OFF
+                -DHDF5_BUILD_EXAMPLES=OFF
+                -DBUILD_TESTING=OFF
+                -DCMAKE_C_FLAGS="-w"
+                -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+            SOURCE_DIR
+                ${OWN_HDF5_BUILD}/src/hdf5-${HDF5_VERSION}
+            BINARY_DIR
+                ${OWN_HDF5_BUILD}/build
+            STAMP_DIR
+                ${OWN_HDF5_BUILD}/stamp
+            TMP_DIR
+                ${OWN_HDF5_BUILD}/tmp
+            INSTALL_DIR
+                ""
         )
-        add_library(${target} INTERFACE)
         add_dependencies(${target} built_hdf5)
+
         target_include_directories(${target}
-            INTERFACE
+                PUBLIC
                 ${OWN_HDF5_ROOT}/include
-        )
+                )
         target_link_libraries(${target}
-            INTERFACE
                 ${OWN_HDF5_ROOT}/lib/libhdf5_hl.a
                 ${OWN_HDF5_ROOT}/lib/libhdf5.a
-        )
+                )
     else()
         find_package(HDF5 COMPONENTS C HL)
         if (NOT ${HDF5_FOUND})
             message(FATAL_ERROR "HDF5 not found. Either specify the environment "
-                "variable HDF5_ROOT to guide to a valid HDF5 installation, "
-                "or use the option `-DBUILD_HDF5=ON` to download "
-                "and compile HDF5 automatically."
-            )
+                    "variable HDF5_ROOT to guide to a valid HDF5 installation, "
+                    "or use the option `-DBUILD_HDF5=ON` to download "
+                    "and compile HDF5 automatically."
+                    )
         endif()
-
-        add_library(${target} INTERFACE)
         target_link_libraries(${target} INTERFACE hdf5::hdf5 hdf5::hdf5_hl)
     endif()
-    target_link_libraries(${target} INTERFACE ${CMAKE_DL_LIBS})
 endfunction()
