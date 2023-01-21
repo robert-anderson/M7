bf51f89185bc1678040948ea513f2d797bf699f1
robert-anderson <robert.anderson@kcl.ac.uk>
2023-01-21 04:05:02 +0100

    add unit test for exact C2 and C4

--- src/M7_lib/wavefunction/FciInitializer.cpp
+++ src/M7_lib/wavefunction/FciInitializer.cpp
@@ -5,43 +5,47 @@
 #include <M7_lib/util/ProgressMonitor.h>
 #include "FciInitializer.h"
 #include "M7_lib/foreach/ConnForeachGroup.h"
+#include "M7_lib/field/Mbf.h"
 
 FciInitializer::FciInitializer(const Hamiltonian &h, sys::Particles particles, FciInitOptions opts):
         m_opts(opts), m_is_hermitian(h.is_hermitian()),
         m_mbf_order_table("MBF order table", {mbf_order_row_t(h.m_basis, "mbf")}){
     auto iters = FciIters::make(h, particles, false);
-    const auto count = iters.niter_single();
-    const uint_t count_local = mpi::evenly_shared_count(count);
-    const uint_t displ_local = mpi::evenly_shared_displ(count);
-
     m_mbf_order_table.resize(iters.niter_single());
     buffered::Mbf mbf(h.m_basis);
-    auto setup_fn = [&](){m_mbf_order_table.insert(mbf);};
-    iters.m_single->loop(mbf, setup_fn);
+
+    iters.m_single->loop(mbf, [&](){m_mbf_order_table.insert(mbf);});
+
+    const auto count = m_mbf_order_table.nrow_in_use();
+    const uint_t count_local = mpi::evenly_shared_count(count);
+    const uint_t displ_local = mpi::evenly_shared_displ(count);
 
     conn::Mbf conn(mbf);
     ConnForeachGroup conn_iters(h);
 
     m_sparse_ham.resize(count_local);
 
-    logging::info("Building sparse H matrix ({} rows)", iters.niter_single());
-    ProgressMonitor pm(true, "building sparse H", "basis functions", count_local);
+    logging::info("Building sparse H matrix ({} rows)", count_local);
+    ProgressMonitor pm(true, "building sparse H", "basis functions", count_local, 5);
     auto& row = m_mbf_order_table.m_row;
-    for (row.jump(displ_local); row.in_range(displ_local+count_local); ++row) {
-        const auto& src_mbf = row.m_field;
-        auto& dst_mbf = mbf;
-        const auto irow = row.index()-displ_local;
+    const auto& src_mbf = row.m_field;
+    auto& dst_mbf = mbf;
 
+    auto filling_fn = [&](){
+        const auto helem = h.get_element(src_mbf, conn);
+        if (!ham::is_significant(helem)) return;
+        conn.apply(src_mbf, dst_mbf);
+        auto lookup = m_mbf_order_table.lookup(dst_mbf);
+        DEBUG_ASSERT_TRUE(lookup, "connected MBF is outside generated space");
+        const auto irow = row.index()-displ_local;
+        if (lookup) m_sparse_ham.insert(irow, {lookup.index(), helem});
+    };
+
+    for (row.jump(displ_local); row.in_range(displ_local+count_local); ++row) {
+        const auto irow = row.index()-displ_local;
         const auto helem_diag = h.get_element(src_mbf) + opts.m_diag_shift;
         DEBUG_ASSERT_TRUE(m_sparse_ham[irow].empty(), "sparse row should be empty");
         if (ham::is_significant(helem_diag)) m_sparse_ham.insert(irow, {row.index(), helem_diag});
-        auto filling_fn = [&](){
-            const auto helem = h.get_element(src_mbf, conn);
-            if (!ham::is_significant(helem)) return;
-            conn.apply(src_mbf, dst_mbf);
-            const auto icol = m_mbf_order_table.lookup(dst_mbf).index();
-            m_sparse_ham.insert(irow, {icol, helem});
-        };
         src_mbf.m_decoded.clear();
         conn_iters.loop(conn, src_mbf, filling_fn);
         pm.next();
--- test/test_core/wavefunction/FciInitializer.cpp
+++ test/test_core/wavefunction/FciInitializer.cpp
@@ -6,6 +6,8 @@
 #include <M7_lib/linalg/DenseHamiltonian.h>
 #include <M7_lib/hamiltonian/frm/J1J2FrmHam.h>
 #include "test_core/defs.h"
+#include "M7_lib/field/Mbf.h"
+#include "M7_lib/mae/MaeTable.h"
 
 #ifdef ENABLE_FERMIONS
 TEST(FciInitializer, N2) {
@@ -21,6 +23,100 @@ TEST(FciInitializer, N2) {
     results.get_eval(0, eval);
     ASSERT_NEAR_EQ(eval, dense_evals[0]);
 }
+
+TEST(FciInitializer, C4) {
+    GeneralFrmHam frm_ham({"FCIDUMP"});
+    Hamiltonian ham(&frm_ham);
+    FciInitOptions opt;
+    opt.m_ritz_tol = 1e-7;
+    opt.m_niter_max = 1000;
+    opt.m_nroot = 2;
+    opt.m_diag_shift = -4;
+    ham_comp_t eval;
+    FciInitializer init(ham, opt);
+    auto results = init.solve();
+
+
+    buffered::MappedTable<MaeRow> c1("c1", {opsig::c_1100, 1ul});
+    buffered::MappedTable<MaeRow> c2("c2", {opsig::c_2200, 1ul});
+    buffered::MappedTable<MaeRow> c3("c3", {opsig::c_3300, 1ul});
+    buffered::MappedTable<MaeRow> c4("c4", {opsig::c_4400, 1ul});
+
+    wf_t nw_tot = 0.0;
+    v_t<wf_t> nw_sectors(5, 0.0);
+
+    if (mpi::i_am_root()) {
+
+        results.get_eval(0, eval);
+        logging::info("FCI energy: {}", eval);
+
+        buffered::Mbf hf(ham.m_basis);
+        const auto elecs = ham.default_particles().m_frm;
+        mbf::set_aufbau_mbf(hf, elecs);
+        conn::Mbf conn(hf);
+
+        c1.resize(integer::combinatorial(
+                2 * hf.m_basis.m_nsite - elecs, 1ul) * integer::combinatorial(uint_t(elecs), 1ul));
+        c2.resize(integer::combinatorial(
+                2 * hf.m_basis.m_nsite - elecs, 2ul) * integer::combinatorial(uint_t(elecs), 2ul));
+        c3.resize(integer::combinatorial(
+                2 * hf.m_basis.m_nsite - elecs, 3ul) * integer::combinatorial(uint_t(elecs), 3ul));
+        c4.resize(integer::combinatorial(
+                2 * hf.m_basis.m_nsite - elecs, 4ul) * integer::combinatorial(uint_t(elecs), 4ul));
+        buffered::MaeInds c1_inds(opsig::c_1100);
+        buffered::MaeInds c2_inds(opsig::c_2200);
+        buffered::MaeInds c3_inds(opsig::c_3300);
+        buffered::MaeInds c4_inds(opsig::c_4400);
+        auto evec = results.get_evec(0);
+        wf_t c0 = *evec;
+        nw_sectors[0] = 1.0;
+        const auto thresh = 1e-12;
+        auto row = init.m_mbf_order_table.m_row;
+        --evec;
+
+        for (row.restart(); row; ++row) {
+            ++evec;
+            if (std::abs(c0) < std::abs(*evec)) {
+                c0 = *evec;
+                hf = row.m_field;
+            }
+        }
+        logging::info("ref det: {}", hf.to_string());
+        evec = results.get_evec(0);
+        --evec;
+
+        v_t<std::pair<MaeIndsField*, MappedTable<MaeRow>*>> pairs = {
+                {nullptr,  nullptr},
+                {&c1_inds, &c1},
+                {&c2_inds, &c2},
+                {&c3_inds, &c3},
+                {&c4_inds, &c4}
+        };
+
+        for (row.restart(); row; ++row) {
+            ++evec;
+            if (std::abs(*evec) < thresh) continue;
+            nw_tot += std::abs(*evec/c0);
+            conn.connect(hf, row.m_field);
+            const auto lvl = conn.exsig().nfrm_cre();
+            if (!lvl) continue;
+            if (lvl >= pairs.size()) continue;
+
+            nw_sectors[lvl] += std::abs(*evec/c0);
+            auto& inds = *pairs[lvl].first;
+            auto& table = *pairs[lvl].second;
+            inds = conn;
+            table.insert(inds).m_values = *evec / c0;
+        }
+    }
+
+    hdf5::FileWriter fw("ci.h5");
+    c2.save(fw, "c2", mpi::i_am_root());
+    c4.save(fw, "c4", mpi::i_am_root());
+    fw.save_dataset("nw_tot", nw_tot, mpi::i_am_root());
+    fw.save_dataset("nw_sectors", nw_sectors, mpi::i_am_root());
+}
+
 
 TEST(FciInitializer, J1J2) {
     J1J2FrmHam frm_ham(0.25, lattice::make("ortho", {16}, {1}));
