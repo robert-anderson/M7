1ec5a459fabc70652e83f8af765301d49bc08551
Robert Anderson <robert.anderson@kcl.ac.uk>
2023-03-29 14:19:33 +0200

    renaming system test directory to ci_pmntr

--- system_test/abinitio/wf_save_load/serial/test.py
+++
@@ -1,9 +0,0 @@
-from system_test import *
-
-skip_if('many-body basis function', 'boson (permanent)')
-run('config.save.yaml', assets=['HF_RDMs/FCIDUMP'])
-exact_e0 =-99.9421389039332
-check_nw()
-check_ninit()
-#check_shift_correct(exact_e0, 10000)
-#check_proje_correct(exact_e0, 10000)
