79e77806884787c9b7d80acdb2795fb6db5ee69c
robert-anderson <robert.anderson@kcl.ac.uk>
2023-06-28 15:41:50 +0200

    amend check_* function calls

--- system_test/frm/real/abinitio/rdms/frm_ranks_1_2_3/serial/no_detsub_refresh/test.py
+++ system_test/frm/real/abinitio/rdms/frm_ranks_1_2_3/serial/no_detsub_refresh/test.py
@@ -9,7 +9,7 @@ compare_nw()
 compare_rdm_archives()
 
 exact_e0 = -99.94213890
-check_shift(exact_e0, 10000)
-check_proje(exact_e0, 10000)
+check_shift(exact_e0)
+check_proje(exact_e0)
 
 check_spinfree_rdms('M7.rdm.h5', 'HF_RDMs/exact_rdms.pkl', ('1100', '2200', '3300'))
