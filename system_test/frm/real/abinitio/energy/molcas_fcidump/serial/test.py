32711d154fca140c3cab716b10984393fd179e6d
robert-anderson <robert.anderson@kcl.ac.uk>
2023-07-18 14:22:05 +0200

    rebenchmarking after removal of grace period

--- system_test/frm/real/abinitio/energy/molcas_fcidump/serial/test.py
+++ system_test/frm/real/abinitio/energy/molcas_fcidump/serial/test.py
@@ -8,4 +8,4 @@ run(link_deps=[('N2_Molcas/molcas.FciDmp.h5', 'FCIDUMP')])
 exact_e0 = -109.02180323
 compare_nw()
 check_shift(exact_e0)
-check_proje(exact_e0)
+check_proje(exact_e0, BlockOpts(npoint=100, nblock=10, err_scale=3.0))
