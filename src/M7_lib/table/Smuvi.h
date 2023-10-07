421acefbb9cd780379d55bfd021a246897d6b677
Robert Anderson <robert.anderson@kcl.ac.uk>
2023-10-07 17:42:34 +0100

    more SMUVI implementation

--- src/M7_lib/table/Smuvi.h
+++ src/M7_lib/table/Smuvi.h
@@ -9,8 +9,41 @@
 #include "BufferedTable.h"
 
 /**
- * Shared Memory Unordered map to Vectors of Indices(/Items)
+ * Shared Memory Unordered map to Vectors of Indices(/Items) "SMUVI"
+ * The SMUVI is a type of parallel hash map from a key domain to a variable-length vector of indices which has fewer
+ * requirements than a parallelised generalization of a map from type T to a arbitrarily sized array of type U elements,
+ * expressed using STL containers as std::unordered_map<T, std::vector<unsigned long>>.
+ *
+ * Primary among which is that the map is never simultaneously modified and read; it is at any given time in one of two
+ * "insert" or "access"
+ *
+ * The required data structure need not support some operations commonly implemented in serial and concurrent hash maps:
+ *  - Reassignment of existing elements. Newly inserted scalar values are appended to the existing list of values
+ *    corresponding to the key
+ *  - Deletion. No key or element of a value array may be removed after it has been added
+ *  - Dynamic resizing of the "access" key array.
+ *
+ * The SMUVI behaves as a hash table which can be inserted to in parallel, then __collated__ with minimal
+ * synchronisation overhead, to then be accessed for reading in constant time among all ranks in a communicator with a
+ * common shared memory window.
+ *
+ * In the insert state, each rank initialises a private hash map and this is filled according to the workload of
+ * insertions allotted to the rank. For access, shared-memory implementations of hash maps usually need to allow for the
+ * possibility that a bin of keys can be written to or deleted from while the read operation is being carried out, and
+ * as such a lock must be acquired before a read can take place, which results in sychronisation overhead. However, if
+ * the SMUVI is in access state, each rank in a shared memory realm can lookup elements without the need to lock a
+ * region of the buffer.
+ *
+ * The SMUVI state is transformed from insert to access in the collate operation. This proceeds by collecting the keys
+ * from each insertion map into a local array which is then sorted. These sorted local keys are then gathered
+ * contiguously into a shared memory buffer. The values are also transferred. The gathered keys are then globally sorted
+ * via a K-way merge and duplicate keys are eliminated
+ *
+ * 1. each rank initializes a private MappedTable<row_t>, in which keys of the type row_t::key_t index the entries
+ * 2. when a previously un-inserted
+ *
  */
+
 namespace smuvi {
     template<typename row_t>
     struct LocalTable : MappedTable<row_t> {
--- test/test_core/table/Smuvi.cpp
+++ test/test_core/table/Smuvi.cpp
@@ -5,6 +5,8 @@
 #include "test_core/defs.h"
 #include "M7_lib/table/Smuvi.h"
 #include "M7_lib/table/BufferedFields.h"
+#include "M7_lib/communication/Communicator.h"
+#include "M7_lib/foreach/BasicForeach.h"
 
 TEST(Smuvi, LocalTable) {
     typedef SingleFieldRow<field::Number<uint_t>> row_t;
@@ -32,4 +34,122 @@ TEST(Smuvi, LocalTable) {
         ASSERT_TRUE(inds_ptr);
         ASSERT_EQ(*inds_ptr, combined_pairs[key]);
     }
+}
+
+
+template<typename key_t>
+struct Smuvi {
+
+    struct InsertRow : Row {
+        key_t m_key;
+        field::Number<uint_t> m_entry;
+
+        template<typename ...Args>
+        InsertRow(const Args&... key_ctor_args): m_key(this, key_ctor_args...), m_entry(this){}
+
+        key_t &key_field() {
+            return m_key;
+        };
+    };
+
+    communicator::BasicSend<InsertRow, InsertRow> m_inserter;
+    v_t<buffered::MappedTable<InsertRow>> m_accessors;
+
+    template<typename ...Args>
+    Smuvi(str_t name, const Args&... key_ctor_args):
+        m_inserter(
+            name,
+            InsertRow(key_ctor_args...),
+            DistribOptions(),
+            {100, 1.0},
+            InsertRow(key_ctor_args...),
+            {100, 1.0}){
+
+//        for (uint_t irank=0ul; irank < mpi::nrank(); ++irank) {
+//
+//        }
+//        m_accessors(name, InsertRow(key_ctor_args...), true) {}
+//
+//        m_accessors(name, InsertRow(key_ctor_args...), true)
+
+    }
+
+
+
+    void insert(const key_t& key, const uint_t& entry) {
+        const auto irank_dst = m_inserter.m_dist.irank(key);
+        Table<InsertRow>& send = m_inserter.m_send_recv.send(irank_dst);
+        send.m_row.push_back_jump();
+        send.m_row.m_key = key;
+        send.m_row.m_entry = entry;
+    }
+
+    void collate() {
+        m_inserter.communicate();
+//        for (auto&)
+    }
+
+
+
+};
+TEST(Smuvi, Comms) {
+    const sys::frm::Basis basis(4);
+    const uint_t nelec_per_channel = 2;
+
+    /*
+     * generate and store all the single-channel configurations for spin-conserving ONV enumeration
+     */
+    v_t<uintv_t> all_channel_setbits;
+    {
+        auto fn = [&](const uintv_t& inds){all_channel_setbits.push_back(inds);};
+        basic_foreach::rtnd::Ordered<true, true> foreach(basis.m_nsite, nelec_per_channel);
+        foreach.loop(fn);
+    }
+
+    const auto nelem = all_channel_setbits.size() * all_channel_setbits.size();
+    const uint_t max_nentry = 5;
+    const uint_t max_entry = 20;
+
+    const auto elem_entry_counts = hash::in_range<uint_t>(0, nelem, 0, max_nentry);
+    v_t<uintv_t> elem_entries;
+    elem_entries.reserve(nelem);
+    for (uint_t ielem = 0; ielem < nelem; ++ielem) {
+        const auto nentry = elem_entry_counts[ielem];
+        elem_entries.emplace_back(hash::unique_in_range<uint_t>(ielem, nentry, 0, max_entry));
+    }
+
+    v_t<std::pair<uintp_t, uint_t>> input_data;
+
+    uint_t ielem = 0;
+    for (uint_t ialpha = 0; ialpha < all_channel_setbits.size(); ++ialpha) {
+        for (uint_t ibeta = 0; ibeta < all_channel_setbits.size(); ++ibeta) {
+            for (const auto& entry: elem_entries[ielem]) {
+                input_data.push_back({{ialpha, ibeta}, entry});
+            }
+            ++ielem;
+        }
+    }
+
+    /*
+     * shuffle the input data to arbitrary order
+     */
+    const auto nshuffle = input_data.size();
+    for (uint_t ishuffle = 0; ishuffle < nshuffle; ++ishuffle) {
+        auto shuffle_pair = hash::in_range(ishuffle, 2, 0, input_data.size());
+        std::swap(input_data[shuffle_pair[0]], input_data[shuffle_pair[1]]);
+    }
+
+    Smuvi<field::FrmOnv> smuvi("my_smuvi", basis);
+    buffered::FrmOnv mbf(basis);
+
+    for (const auto& data: input_data) {
+        const auto& alpha_string = all_channel_setbits[data.first.first];
+        const auto& beta_string = all_channel_setbits[data.first.second];
+        const auto& entry = data.second;
+        mbf = {alpha_string, beta_string};
+        smuvi.insert(mbf, entry);
+    }
+    smuvi.collate();
+
+    std::cout << smuvi.m_inserter.m_send_recv.recv().to_string() << std::endl;
 }
