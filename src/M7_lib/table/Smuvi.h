a360e2571f9bc2a7f7b812452f57b67a94e05b3a
Robert Anderson <robert.anderson@kcl.ac.uk>
2024-02-26 08:53:16 +0000

    implement unordered smuvi

--- src/M7_lib/table/Smuvi.h
+++ src/M7_lib/table/Smuvi.h
@@ -372,8 +372,59 @@ public:
         }
     };
 
+    struct EqFn {
+        const InsertRow m_row_1;
+        const InsertRow m_row_2;
+
+        EqFn(const send_recv::BasicSend<InsertRow>& inserter):
+                m_row_1(inserter.recv().m_row), m_row_2(inserter.recv().m_row) {}
+
+        EqFn(const EqFn& other): m_row_1(other.m_row_1), m_row_2(other.m_row_2) {}
+
+        EqFn& operator=(const EqFn& other) {
+            m_row_1.jump(other.m_row_1.index());
+            m_row_2.jump(other.m_row_2.index());
+            return *this;
+        }
+
+        bool operator()(uint_t i, uint_t j) const {
+            m_row_1.jump(i);
+            m_row_2.jump(j);
+            return m_row_1.m_value == m_row_2.m_value;
+        }
+    };
+
+    struct HashFn {
+        const InsertRow m_row;
+
+        HashFn(const send_recv::BasicSend<InsertRow>& inserter): m_row(inserter.recv().m_row){}
+
+        HashFn(const HashFn& other): m_row(other.m_row) {}
+
+        HashFn& operator=(const HashFn& other) {
+            m_row.jump(other.m_row.index());
+            return *this;
+        }
+
+        uint_t operator()(uint_t i) const {
+            m_row.jump(i);
+            return m_row.m_value.hash();
+        }
+    };
+private:
+
     template<typename fn_t>
-    void collate(const fn_t& order_fn) {
+    std::set<uint_t, CompFn<fn_t>> empty_container(const fn_t& order_fn) {
+        return std::set<uint_t, CompFn<fn_t>>(CompFn<fn_t>(m_inserter, order_fn));
+    }
+
+    std::unordered_set<uint_t, HashFn, EqFn> empty_container(const std::false_type&) {
+        return std::unordered_set<uint_t, HashFn, EqFn>(1, HashFn(m_inserter), EqFn(m_inserter));
+    }
+
+protected:
+    template<typename fn_t>
+    void collate_base(const fn_t& order_fn) {
         /*
          * first send the inserted key-index pairs to the receiving ranks
          */
@@ -384,11 +435,18 @@ public:
          */
         OpenAddressedTable<AccessRow>& accessor = m_access_tables[m_irank_world_to_iaccess_table[mpi::irank()]];
         /*
+         * the type of the auxiliary container is dependent on whether the order_fn has been specified.
+         * all auxiliary containers ensure uniqueness of values, but only when fn_t is not std::false_type does it
+         * guarantee ordered value sets.
+         */
+        using cont_t = typename std::conditional<std::is_same<fn_t, std::false_type>::value,
+                std::unordered_set<uint_t, HashFn, EqFn>, std::set<uint_t, CompFn<fn_t>>>::type;
+        /*
          * initialize a private set of indices for each key sent to this rank. The sets are used to build up an
          * unordered list of the value element indices in the recv table associated with each key in the accessor,
          * which will later be copied into the shared memory m_entries arrays
          */
-        v_t<std::set<uint_t, CompFn<fn_t>>> value_index_sets;
+        v_t<cont_t> value_index_sets;
 
         /*
          * loop over the received rows
@@ -418,7 +476,7 @@ public:
              * if there aren't enough value index vector sets for the current size of the accessor, allocate more
              */
             if (value_index_sets.size() < accessor.nrow_in_use()) {
-                value_index_sets.resize(accessor.nrow_in_use(), std::set<uint_t, CompFn<fn_t>>(CompFn<fn_t>(m_inserter, order_fn)));
+                value_index_sets.resize(accessor.nrow_in_use(), empty_container(order_fn));
             }
             /*
              * get the indices vector to recv_row.m_key, and append the new index
@@ -514,140 +572,6 @@ public:
         for (auto& table: m_values_tables) table.end_sync();
         mpi::barrier();
     }
-
-#if 0
-    void collate_nosort() {
-        /*
-         * first send the inserted key-index pairs to the receiving ranks
-         */
-        m_inserter.communicate();
-        /*
-         * a shared memory MappedTable is set up for each rank, so that they can be set up without dataraces by a single
-         * rank, and thereafter accessed by all ranks in the shared memory region
-         */
-        MappedTable<AccessRow>& accessor = m_access_tables[m_irank_world_to_iaccess_table[mpi::irank()]];
-        /*
-         * initialize a private set of indices for each key sent to this rank. The sets are used to build up an
-         * unordered list of the value element indices in the recv table associated with each key in the accessor,
-         * which will later be copied into the shared memory m_entries arrays
-         */
-        v_t<std::unordered_set<uint_t>> value_index_sets;
-
-        /*
-         * loop over the received rows
-         */
-        auto& recv_row = m_inserter.recv().m_row;
-
-        {
-            auto sizes = mpi::all_gathered(m_inserter.recv().nrow_in_use());
-            auto size_it = sizes.cbegin();
-            REQUIRE_EQ_ALL(sizes.size(), m_access_tables.size(), "size mismatch");
-            for (auto& table : m_access_tables) {
-                table.resize(*size_it++);
-            }
-        }
-        for (recv_row.restart(); recv_row; ++recv_row) {
-            /*
-             * lookup the received key in the accessor table, or insert it if this is the first instance
-             */
-            accessor.remap_if_due();
-            auto& accessor_row = accessor.lookup_or_insert(recv_row.m_key);
-            /*
-             * if there aren't enough value index vector sets for the current size of the accessor, allocate more, this
-             * can be validly done inside the loop, since there is no shared memory for v_t<std::set>
-             */
-            if (value_index_sets.size() < accessor.nrecord())
-                value_index_sets.resize(accessor.nrecord(), std::unordered_set<uint_t>());
-            /*
-             * get the indices vector to recv_row.m_key, and append the new index
-             */
-            value_index_sets[accessor_row.index()].insert(recv_row.index());
-        }
-
-        /*
-         * a vector storing the number of entry_sets associated with each MPI rank
-         */
-        uintv_t nentries;
-        {
-            // the displacement from the beginning of the entry_sets
-            uint_t displ = 0ul;
-            /*
-             * iterator initially pointing to the beginning of the entry sets (i.e. the first key in the accessor
-             * created by this rank)
-             */
-            auto value_index_set_it = value_index_sets.cbegin();
-            // loop over all the rows in the accessor created by this rank
-            auto& accessor_row = accessor.m_row;
-            for (accessor_row.restart(); accessor_row; ++accessor_row) {
-                /*
-                 * set the displacement that will denote the index in the entry_sets array at which the entry_sets
-                 * corresponding to the current row in the accessor (i.e. the key) begin
-                 */
-                accessor_row.m_value_displ = displ;
-                /*
-                 * set the number of entry_sets corresponding to the key
-                 */
-                accessor_row.m_value_count = value_index_set_it->size();
-                /*
-                 * increment the rank-private displ count
-                 */
-                displ += value_index_set_it->size();
-                /*
-                 * advance the entry iterator
-                 */
-                ++value_index_set_it;
-            }
-            REQUIRE_TRUE_ALL(value_index_set_it == value_index_sets.cend(), "should have iterated through all entry sets");
-            /*
-             * gather the final displs (i.e. the total number of unique values across all keys sent to this rank)
-             */
-            mpi::all_gather(displ, nentries);
-        }
-
-        /*
-         * clear the entries shared memory arrays in case this is not the first call to collate
-         */
-        for (auto& values_table: m_values_tables) values_table.clear();
-
-        {
-            auto size_it = nentries.cbegin();
-            REQUIRE_EQ_ALL(nentries.size(), m_values_tables.size(), "size mismatch");
-            for (auto& table : m_values_tables) {
-                table.resize(*size_it++);
-            }
-        }
-
-        /*
-         * now write the sets of entries stored privately on this rank to the shared memory arrays accessible by all
-         * ranks on the same shared memory region
-         */
-        {
-            /*
-             * get a reference to the values array updated by this rank
-             */
-            auto& values_table = m_values_tables[m_irank_world_to_iaccess_table[mpi::irank()]];
-            DEBUG_ASSERT_TRUE(values_table.empty(), "value tables should be been cleared");
-            values_table.push_back(nentries[mpi::irank()]);
-            uint_t ielement = 0;
-            auto& value_row = values_table.m_row;
-            for (const auto& value_index_set : value_index_sets) {
-                /*
-                 * loop over all value indices in all sets, copying the indexed values contiguously to the values table
-                 */
-                for (const auto& i: value_index_set) {
-                    recv_row.jump(i);
-                    value_row.jump(ielement++);
-                    value_row.m_value = recv_row.m_value;
-                }
-            }
-        }
-        /*
-         * now the accessor tables are updated with the keys, and the entries arrays are updated with their
-         * corresponding sorted and unique index values
-         */
-    }
-#endif
-
 };
 
 /**
@@ -693,6 +617,11 @@ public:
     void foreach_common_value(const key_t& key_1, const key_t& key_2, const fn_t& fn) const {
         foreach_common_value(key_1, *this, key_2, fn);
     }
+
+    template<typename fn_t>
+    void collate(const fn_t& order_fn) {
+        SmuviBase<key_t, value_t>::collate_base(order_fn);
+    }
 };
 
 /**
@@ -702,6 +631,10 @@ template<typename key_t, typename value_t>
 class UnorderedSmuvi : public SmuviBase<key_t, value_t> {
 public:
     UnorderedSmuvi(str_t name, const key_t& key, const value_t& value): SmuviBase<key_t, value_t>(std::move(name), key, value){}
+
+    void collate() {
+        SmuviBase<key_t, value_t>::collate_base(std::false_type());
+    }
 };
 
 #endif //M7_SMUVI_H
