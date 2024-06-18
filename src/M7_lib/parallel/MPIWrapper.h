ac549f7ce234f3b88c988baaaeb12561bcae0ec4
Arta Safari <a.safari@fkf.mpg.de>
2024-06-18 09:37:57 +0200

    num_message superfluous

--- src/M7_lib/parallel/MPIWrapper.h
+++ src/M7_lib/parallel/MPIWrapper.h
@@ -808,27 +808,12 @@ namespace mpi {
     static bool all_to_allv(
             const T *send, const uintv_t &sendcounts, const uintv_t &senddispls,
             T *recv, const uintv_t &recvcounts, const uintv_t &recvdispls) {
-
-        // check whether message surpasses MPI integer range and dispatch large count AllToAllV_c in that case
-        double total_send = 0.0;
-        for (const auto &data : sendcounts) total_send += data;
-        uint_t num_message = ceil(total_send / std::numeric_limits<count_t>::max());
-
-        // if (num_message > 1) {
         auto tmp_sendcounts = convert::safe_narrow<MPI_Count>(sendcounts);
         auto tmp_recvcounts = convert::safe_narrow<MPI_Count>(recvcounts);
         auto tmp_senddispls = convert::safe_narrow<MPI_Aint>(senddispls);
         auto tmp_recvdispls = convert::safe_narrow<MPI_Aint>(recvdispls);
         return all_to_allv_c(send, tmp_sendcounts.data(), tmp_senddispls.data(), recv,
                              tmp_recvcounts.data(), tmp_recvdispls.data());
-        // } else {
-        //     auto tmp_sendcounts = snrw(sendcounts);
-        //     auto tmp_senddispls = snrw(senddispls);
-        //     auto tmp_recvcounts = snrw(recvcounts);
-        //     auto tmp_recvdispls = snrw(recvdispls);
-        //     return all_to_allv(send, tmp_sendcounts.data(), tmp_senddispls.data(), recv,
-        //                        tmp_recvcounts.data(), tmp_recvdispls.data());
-        // }
     }
 
     bool initialized();
