7ea1fd6070717b0038f709087b056721a5a3d48d
Arta Safari <a.safari@fkf.mpg.de>
2024-07-02 18:00:35 +0200

    retain more F|0> WF elements

--- src/M7_lib/mae/SpinMapRdmFiller.h
+++ src/M7_lib/mae/SpinMapRdmFiller.h
@@ -740,7 +740,7 @@ public:
             wf_comp_t discarded_norm = 0.0;
             auto screen_fock_fn = [&](const MbfWeightRow &fock_row){
                 f4rdm_norm += std::abs(fock_row.m_weight[0]);
-                if (std::abs(fock_row.m_weight[0]) > f_x_hist_compress_thresh / 2) {
+                if (std::abs(fock_row.m_weight[0]) > f_x_hist_compress_thresh / 10) {
                     screened_row.push_back_jump();
                     screened_row.m_mbf = fock_row.m_mbf;
                     screened_row.m_weight = fock_row.m_weight;
