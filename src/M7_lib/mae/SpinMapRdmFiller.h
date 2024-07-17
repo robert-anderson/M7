562448470cc6aa954a58dd0d75e384a72557fe86
Arta Safari <a.safari@fkf.mpg.de>
2024-07-17 07:39:16 +0200

    clear receive rows after communication to save memory

--- src/M7_lib/mae/SpinMapRdmFiller.h
+++ src/M7_lib/mae/SpinMapRdmFiller.h
@@ -374,7 +374,7 @@ class SpinMapRdmFiller {
             dst.m_weight += recv_row.m_weight;
             psi1.m_store.remap_if_due();
         }
-        /* these receive rows take a lot of memory, clear them after use */
+        /* receive rows take a lot of memory, clear them after use */
         psi1.m_send_recv.recv().clear();
     }
 
