0bb9f84eaf1680f79f94c38bf4543be9f16d9bee
Arta Safari <a.safari@fkf.mpg.de>
2024-10-07 09:23:41 +0200

    add comment regarding row estimates in Rdm.cpp

--- src/M7_lib/bilinear/Rdm.cpp
+++ src/M7_lib/bilinear/Rdm.cpp
@@ -13,6 +13,10 @@ uint_t Rdm::nrow_estimate(uint_t nfrm_cre, uint_t nfrm_ann, uint_t nbos_cre, uin
     nrow *= integer::combinatorial(basis_size.m_frm.m_nspinorb, nfrm_ann);
     nrow *= integer::combinatorial_with_repetition(basis_size.m_bos, nbos_cre);
     nrow *= integer::combinatorial_with_repetition(basis_size.m_bos, nbos_ann);
+    /**
+     * Purely empirical estimates for single node, 126 core calculations.
+     * Physically motivated estimates were either too large or displayed poor performance.
+     */
     nrow /= nfrm_cre + nfrm_ann + 1;
     nrow /= nbos_cre + nbos_ann + 1;
     if (nfrm_cre > 2 || nfrm_ann > 2) nrow /= 18;
